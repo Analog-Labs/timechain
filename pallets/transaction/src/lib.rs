2cfeba10e1b8e2480ca92af1c9fc90b1145ec395
Ali Janjua <alinasir555@gmail.com>
2022-03-30 12:51:08 +0500

    implementation of time standard as per latest standard

--- pallets/transaction/src/lib.rs
+++ pallets/transaction/src/lib.rs
@@ -37,11 +37,29 @@ pub mod pallet {
 	#[cfg(feature = "std")]
 	use serde::{Deserialize, Serialize};
 
-	pub(crate) const ERR_INVALID_EVENT_NAME: u8 = 101;
-	pub(crate) const ERR_INVALID_EVENT_CATEGORY: u8 = 102;
-	pub(crate) const ERR_INVALID_EVENT_SUBJECT: u8 = 103;
-	pub(crate) const ERR_INVALID_EVENT_OBJECT: u8 = 104;
-	pub(crate) const ERR_INVALID_RECEIVER: u8 = 105;
+	// Error codes, used during validation before the transaction enters tx pool.
+
+	// range: 101 - 120
+	pub(crate) const ERR_INVALID_SOURCE_CHAIN_ID: u8 = 101;
+	pub(crate) const ERR_INVALID_DEST_CHAIN_ID: u8 = 102;
+	//pub(crate) const ERR_INVALID_EVENT_HASH: u8 = 103;
+	pub(crate) const ERR_INVALID_TESSARACT_HASH: u8 = 104;
+	//pub(crate) const ERR_INVALID_TX_HASH_BY_SOURCE: u8 = 105;
+	//pub(crate) const ERR_INVALID_TX_HASH_BY_ACCOUNT: u8 = 106;
+	//pub(crate) const ERR_INVALID_TX_HASH_BY_TESSARACT: u8 = 107;
+
+	// range: 121 - 140
+	pub(crate) const ERR_INVALID_EVENT_PUBLISHER: u8 = 121;
+	pub(crate) const ERR_INVALID_EVENT_NAME: u8 = 122;
+	pub(crate) const ERR_INVALID_EVENT_CATEGORY: u8 = 123;
+	pub(crate) const ERR_INVALID_EVENT_DATA: u8 = 124;
+	//pub(crate) const ERR_INVALID_TIMESTAMP: u8 = 125;
+
+	// range: 141 - 160
+	//pub(crate) const ERR_INVALID_THRESHOLD_SIGNATURE: u8 = 141;
+
+	// range: 161 - 180
+	//pub(crate) const ERR_INVALID_RECEIVER: u8 = 161;
 
 	/// Configure the pallet by specifying the parameters and types on which it depends.
 	#[pallet::config]
@@ -52,6 +70,7 @@ pub mod pallet {
 	}
 
 	// Transaction struct
+	// https://www.notion.so/Time-chain-Standard-5154e1d1ba6341b494a88e16aa35b522
 	#[cfg_attr(feature = "std", derive(Serialize, Deserialize))]
 	#[derive(
 		PartialEq, Eq, PartialOrd, Ord, Default, Clone, Encode, Decode, Hash, Debug, TypeInfo,
@@ -73,8 +92,13 @@ pub mod pallet {
 		PartialEq, Eq, PartialOrd, Ord, Default, Clone, Encode, Decode, Hash, Debug, TypeInfo,
 	)]
 	pub struct TxHeader {
+		pub source_chain_id: u32,
+		pub destination_chain_id: u32,
 		pub event_hash: H256,
-		pub previous_hash: H256,
+		pub tessaract_hash: H256,
+		pub previous_tx_hash_by_publisher: H256,
+		pub previous_tx_hash_by_account: H256,
+		pub previous_tx_hash_by_tessaract: H256,
 	}
 
 	impl MaxEncodedLen for TxHeader {
@@ -88,11 +112,11 @@ pub mod pallet {
 		PartialEq, Eq, PartialOrd, Ord, Default, Clone, Encode, Decode, Hash, Debug, TypeInfo,
 	)]
 	pub struct TxBody {
+		pub event_publisher: H256,
 		pub event_name: Vec<u8>,
-		pub category: Vec<u8>,
-		pub subject: H256,
-		pub object: H256,
-		pub time_attributes: u128,
+		pub event_category: Vec<u8>,
+		pub event_data: Vec<u8>,
+		pub timestamp: u128,
 	}
 
 	impl MaxEncodedLen for TxBody {
@@ -106,7 +130,7 @@ pub mod pallet {
 		PartialEq, Eq, PartialOrd, Ord, Default, Clone, Encode, Decode, Hash, Debug, TypeInfo,
 	)]
 	pub struct TxFooter {
-		pub broadcaster_signature: H512,
+		pub threshold_signature: H512,
 	}
 
 	impl MaxEncodedLen for TxFooter {
@@ -142,25 +166,48 @@ pub mod pallet {
 		}
 	}
 
+	// Transaction structure wrapped inside Tx
+	// Storing the transaction directly causes DefaultTrait missing error.
+	#[cfg_attr(feature = "std", derive(Serialize, Deserialize))]
+	#[derive(
+		PartialEq, Eq, PartialOrd, Ord, Default, Clone, Encode, Decode, Hash, Debug, TypeInfo,
+	)]
+	pub struct Tx {
+		pub transaction: Transaction,
+	}
+	impl MaxEncodedLen for Tx {
+		fn max_encoded_len() -> usize {
+			usize::MAX
+		}
+	}
+
 	#[pallet::pallet]
 	#[pallet::generate_store(pub(super) trait Store)]
 	pub struct Pallet<T>(_);
 
-	// The pallet's runtime storage items.
-	// https://docs.substrate.io/v3/runtime/storage
 	#[pallet::storage]
-	// Learn more about declaring storage items:
-	// https://docs.substrate.io/v3/runtime/storage#declaring-storage-items
-	pub(super) type Transactions<T: Config> =
-		StorageMap<_, Blake2_128Concat, Transaction, (H256, T::BlockNumber), ValueQuery>;
+	#[pallet::getter(fn get_tx_hash_by_event_hash)]
+	pub(super) type TxHashByEvent<T: Config> =
+		StorageMap<_, Blake2_128Concat, H256, H256, ValueQuery>;
 
 	#[pallet::storage]
-	pub(super) type Transferes<T: Config> =
-		StorageMap<_, Blake2_128Concat, T::AccountId, TransactionList, ValueQuery>;
+	#[pallet::getter(fn get_tx_hash_by_account_hash)]
+	pub(super) type TxHashByAccount<T: Config> =
+		StorageMap<_, Blake2_128Concat, H256, H256, ValueQuery>;
 
 	#[pallet::storage]
-	pub(super) type PreviousHash<T: Config> =
-		StorageMap<_, Blake2_128Concat, T::AccountId, PreviousHashes, ValueQuery>;
+	#[pallet::getter(fn get_tx_hash_by_tessaract_hash)]
+	pub(super) type TxHashByTessaract<T: Config> =
+		StorageMap<_, Blake2_128Concat, H256, H256, ValueQuery>;
+
+	#[pallet::storage]
+	#[pallet::getter(fn get_tx_hash_by_publisher_hash)]
+	pub(super) type TxHashByPublisher<T: Config> =
+		StorageMap<_, Blake2_128Concat, H256, H256, ValueQuery>;
+
+	#[pallet::storage]
+	#[pallet::getter(fn get_tx_by_hash)]
+	pub(super) type Transactions<T: Config> = StorageMap<_, Blake2_128Concat, H256, Tx, ValueQuery>;
 
 	// Pallets use events to inform users when important changes are made.
 	// https://docs.substrate.io/v3/runtime/events-and-errors
@@ -170,6 +217,7 @@ pub mod pallet {
 		EventCreated(T::AccountId, Transaction),
 		EventDeleted(T::AccountId, Transaction),
 		EventTransfered(T::AccountId, T::AccountId, Transaction),
+		EventPublished(Transaction),
 	}
 
 	// Errors inform users that something went wrong.
@@ -178,6 +226,7 @@ pub mod pallet {
 		EventAlreadyExists,
 		NoSuchEvent,
 		NotEventOwner,
+		EventNotPublished,
 	}
 
 	pub fn hardcode_h256() -> H256 {
@@ -200,98 +249,75 @@ pub mod pallet {
 	// Dispatchable functions must be annotated with a weight and must return a DispatchResult.
 	#[pallet::call]
 	impl<T: Config> Pallet<T> {
-		#[pallet::weight(1_1000)]
-		pub fn create_event(origin: OriginFor<T>, transaction: Transaction) -> DispatchResult {
-			let sender = ensure_signed(origin)?;
-
-			// Verify that the specified event has not already been created.
-			ensure!(!Transactions::<T>::contains_key(&transaction), Error::<T>::EventAlreadyExists);
-
-			// Get the block number from the FRAME System pallet.
-			let current_block = <frame_system::Pallet<T>>::block_number();
-
-			// Store the proof with the sender and block number.
-			let mut encoded_account_id = sender.encode();
-			encoded_account_id.resize(32, 0);
-			let h256 = &H256::from_slice(&encoded_account_id[..]);
-			Transactions::<T>::insert(&transaction, (&h256, current_block));
-
-			Self::deposit_event(Event::EventCreated(sender, transaction));
-			Ok(())
-		}
-
-		#[pallet::weight(1_1000)]
-		pub fn delete_event(origin: OriginFor<T>, transaction: Transaction) -> DispatchResult {
-			let sender = ensure_signed(origin)?;
-			let mut encoded_account_id = sender.encode();
-			encoded_account_id.resize(32, 0);
-			let sender_hash = H256::from_slice(&encoded_account_id[..]);
-
-			// Verify that the specified event has been created.
-			ensure!(Transactions::<T>::contains_key(&transaction), Error::<T>::NoSuchEvent);
-
-			// Get owner of the claim.
-			let (owner_hash, _) = Transactions::<T>::get(&transaction);
-
-			// Verify that sender of the current call is the event owner.
-			ensure!(sender_hash == owner_hash, Error::<T>::NotEventOwner);
-
-			// Remove claim from storage.
-			Transactions::<T>::remove(&transaction);
-
-			Self::deposit_event(Event::EventCreated(sender, transaction));
-			Ok(())
-		}
-
-		#[pallet::weight(1_1000)]
-		pub fn transfer_event(
+		#[pallet::weight(100_000)]
+		pub fn publish_event(
 			origin: OriginFor<T>,
-			to: T::AccountId,
+			source_chain_id: u32,
+			destination_chain_id: u32,
+			tessaract_hash: H256,
+			event_publisher: H256,
 			event_name: Vec<u8>,
 			event_category: Vec<u8>,
+			event_data: Vec<u8>,
 		) -> DispatchResult {
-			let sender = ensure_signed(origin)?;
+			let account = ensure_signed(origin)?;
+
+			let account_hash = H256::from_slice(&account.encode()[..]);
+
 			let mut transaction = Transaction {
-				header: TxHeader { event_hash: hardcode_h256(), previous_hash: hardcode_h256() },
-				body: TxBody {
-					event_name,
-					category: event_category,
-					subject: H256::from_slice(&sender.encode()[..]),
-					object: H256::from_slice(&to.encode()[..]),
-					time_attributes: T::TimeProvider::now().as_millis(),
+				header: TxHeader {
+					source_chain_id,
+					destination_chain_id,
+					event_hash: hardcode_h256(),
+					tessaract_hash,
+					previous_tx_hash_by_publisher: hardcode_h256(),
+					previous_tx_hash_by_account: hardcode_h256(),
+					previous_tx_hash_by_tessaract: hardcode_h256(),
 				},
-				footer: TxFooter { broadcaster_signature: hardcore_h512() },
+				body: TxBody {
+					event_publisher,
+					event_name,
+					event_category,
+					event_data,
+					timestamp: T::TimeProvider::now().as_millis(),
+				},
+				footer: TxFooter { threshold_signature: hardcore_h512() },
 			};
 
 			transaction.body.using_encoded(|ref slice| {
 				transaction.header.event_hash = BlakeTwo256::hash(&slice);
-				// In case it is the first event from sender
-				transaction.header.previous_hash = transaction.header.event_hash
 			});
 
-			if PreviousHash::<T>::contains_key(&sender) {
-				// Get the list of preivious hashes from sender
-				let mut previous_hashes = PreviousHash::<T>::get(&sender);
-
-				transaction.header.previous_hash =
-					previous_hashes.hash_list.last().unwrap().clone();
-
-				previous_hashes.hash_list.push(transaction.header.event_hash);
-				PreviousHash::<T>::insert(&sender, previous_hashes);
-			} else {
-				// First transaction, create a new hash_list
-				let mut previous_hashes = PreviousHashes { hash_list: Vec::new() };
-				previous_hashes.hash_list.push(transaction.header.event_hash);
-
-				PreviousHash::<T>::insert(&sender, previous_hashes);
+			// Fetch previous_tx_hash_by_publisher if exists in database
+			if TxHashByPublisher::<T>::contains_key(&event_publisher) {
+				let tx_hash = TxHashByPublisher::<T>::get(&event_publisher);
+				transaction.header.previous_tx_hash_by_publisher = tx_hash;
 			}
 
-			//Get the list of transaction received by an account
-			let mut transaction_list: TransactionList = Transferes::<T>::get(&to);
-			transaction_list.transactions.insert(0, transaction.clone());
-			Transferes::<T>::insert(&to, &transaction_list);
+			// Fetch previous_tx_hash_by_account if exists in database
+			if TxHashByAccount::<T>::contains_key(&account_hash) {
+				let tx_hash = TxHashByAccount::<T>::get(&account_hash);
+				transaction.header.previous_tx_hash_by_account = tx_hash;
+			}
 
-			Self::deposit_event(Event::EventTransfered(sender, to, transaction));
+			// Fetch previous_tx_hash_by_tessaract if exists in database
+			if TxHashByTessaract::<T>::contains_key(&tessaract_hash) {
+				let tx_hash = TxHashByTessaract::<T>::get(&tessaract_hash);
+				transaction.header.previous_tx_hash_by_tessaract = tx_hash;
+			}
+
+			// Calculate tx hash and save it into DB
+			transaction.using_encoded(|ref slice| {
+				let tx_hash = BlakeTwo256::hash(&slice);
+
+				TxHashByEvent::<T>::insert(&transaction.header.event_hash, &tx_hash);
+				TxHashByAccount::<T>::insert(&account_hash, &tx_hash);
+				TxHashByTessaract::<T>::insert(&tessaract_hash, &tx_hash);
+				TxHashByPublisher::<T>::insert(&event_publisher, &tx_hash);
+				Transactions::<T>::insert(&tx_hash, Tx { transaction: transaction.clone() });
+			});
+
+			Self::deposit_event(Event::EventPublished(transaction));
 			Ok(())
 		}
 	}
@@ -299,7 +325,6 @@ pub mod pallet {
 	#[derive(Encode, Decode, Clone, Eq, PartialEq, TypeInfo)]
 	#[scale_info(skip_type_params(T))]
 	pub struct ValidateTransaction<T: Config + Send + Sync>(sp_std::marker::PhantomData<T>);
-
 	impl<T: Config + Send + Sync> sp_std::fmt::Debug for ValidateTransaction<T> {
 		#[cfg(feature = "std")]
 		fn fmt(&self, f: &mut sp_std::fmt::Formatter) -> sp_std::fmt::Result {
@@ -323,22 +348,28 @@ pub mod pallet {
 
 		fn validate(
 			&self,
-			who: &Self::AccountId,
+			_who: &Self::AccountId,
 			call: &Self::Call,
 			_info: &DispatchInfoOf<Self::Call>,
 			_len: usize,
 		) -> TransactionValidity {
 			match call.is_sub_type() {
-				Some(Call::transfer_event { to, event_name, event_category }) => {
-					// More validations comes here...
+				Some(Call::publish_event {
+					source_chain_id,
+					destination_chain_id,
+					tessaract_hash,
+					event_publisher,
+					event_name,
+					event_category,
+					event_data,
+				}) => {
+					validate_source_chain_id(source_chain_id.clone())?;
+					validate_destination_chain_id(destination_chain_id.clone())?;
+					validate_tessaract_hash(tessaract_hash.clone())?;
+					validate_event_publisher(event_publisher.clone())?;
 					validate_event_name(event_name.clone())?;
 					validate_event_category(event_category.clone())?;
-					validate_subject(H256::from_slice(&who.encode()[..]))?;
-					validate_object(H256::from_slice(&to.encode()[..]))?;
-					validate_different_receiver(
-						H256::from_slice(&who.encode()[..]),
-						H256::from_slice(&to.encode()[..]),
-					)
+					validate_event_data(event_data.clone())
 				},
 				_ => Ok(Default::default()),
 			}
@@ -360,6 +391,38 @@ pub mod pallet {
 	}
 
 	// Transaction validations
+	fn validate_source_chain_id(id: u32) -> TransactionValidity {
+		if id <= 0 {
+			Err(InvalidTransaction::Custom(ERR_INVALID_SOURCE_CHAIN_ID).into())
+		} else {
+			Ok(Default::default())
+		}
+	}
+
+	fn validate_destination_chain_id(id: u32) -> TransactionValidity {
+		if id <= 0 {
+			Err(InvalidTransaction::Custom(ERR_INVALID_DEST_CHAIN_ID).into())
+		} else {
+			Ok(Default::default())
+		}
+	}
+
+	fn validate_tessaract_hash(hash: H256) -> TransactionValidity {
+		if hash.as_bytes().len() < 32 {
+			Err(InvalidTransaction::Custom(ERR_INVALID_TESSARACT_HASH).into())
+		} else {
+			Ok(Default::default())
+		}
+	}
+
+	fn validate_event_publisher(hash: H256) -> TransactionValidity {
+		if hash.as_bytes().len() < 32 {
+			Err(InvalidTransaction::Custom(ERR_INVALID_EVENT_PUBLISHER).into())
+		} else {
+			Ok(Default::default())
+		}
+	}
+
 	fn validate_event_name(name: Vec<u8>) -> TransactionValidity {
 		if name.len() <= 0 || name.len() > 20 {
 			Err(InvalidTransaction::Custom(ERR_INVALID_EVENT_NAME).into())
@@ -376,25 +439,9 @@ pub mod pallet {
 		}
 	}
 
-	fn validate_subject(subject: H256) -> TransactionValidity {
-		if subject.as_bytes().len() <= 0 || subject.as_bytes().len() > 32 {
-			Err(InvalidTransaction::Custom(ERR_INVALID_EVENT_SUBJECT).into())
-		} else {
-			Ok(Default::default())
-		}
-	}
-
-	fn validate_object(object: H256) -> TransactionValidity {
-		if object.as_bytes().len() <= 0 || object.as_bytes().len() > 32 {
-			Err(InvalidTransaction::Custom(ERR_INVALID_EVENT_OBJECT).into())
-		} else {
-			Ok(Default::default())
-		}
-	}
-
-	fn validate_different_receiver(from: H256, to: H256) -> TransactionValidity {
-		if to == from {
-			Err(InvalidTransaction::Custom(ERR_INVALID_RECEIVER).into())
+	fn validate_event_data(data: Vec<u8>) -> TransactionValidity {
+		if data.len() > 1024 {
+			Err(InvalidTransaction::Custom(ERR_INVALID_EVENT_DATA).into())
 		} else {
 			Ok(Default::default())
 		}

6eb1b98c1076d9bec5a5aa02dd54f56adc7c7dd5
ArnoldTumukunde <tumukunde@analog.one>
2022-11-14 11:08:31 +0300

    update tests and benchmarking

--- pallets/onchain-task/src/tests.rs
+++ pallets/onchain-task/src/tests.rs
@@ -7,20 +7,16 @@ use frame_system::RawOrigin;
 #[test]
 fn storing_and_get_chain_data() {
 	let chain: SupportedChain = SupportedChain::Timechain;
-	let new_task: ChainTask = ChainTask::SwapToken;
-	let chain_data: ChainData = "this_is_the_chain_data".as_bytes().to_owned();
-	let chain_methods: MethodName = "this_is_the_chain_method".as_bytes().to_owned();
-	let chain_arguments: MethodArguments = "this_is_the_chain_arguments".as_bytes().to_owned();
 	let mut task = Vec::new();
 	let mut task_methods = Vec::new();
 	let task_method = TaskMethod {
-		name: chain_methods,
-		arguments: chain_arguments,
+		name: "this_is_the_chain_method".as_bytes().to_owned(),
+		arguments: "this_is_the_chain_arguments".as_bytes().to_owned(),
 	};
 	task_methods.push(task_method);
 	let onchain_task = TaskData {
-		task: new_task.clone(),
-		chain_data: chain_data.clone(),
+		task:  ChainTask::SwapToken,
+		chain_data: "this_is_the_chain_data".as_bytes().to_owned(),
 		method: task_methods.clone(),
 	};
 	task.push(onchain_task);
@@ -40,54 +36,6 @@ fn storing_and_get_chain_data() {
 		));
 		// Retreiving the task stored via it's key and assert the result.
 		assert_eq!(OnChainTask::task_store(chain.clone()), Some(chain_task.clone()));
-	});
-}
-
-#[test]
-fn edit_chain_task_data() {
-	let chain: SupportedChain = SupportedChain::Timechain;
-	let mut task = Vec::new();
-	let mut task2 = Vec::new();
-	let mut task_methods = Vec::new();
-	let task_method = TaskMethod {
-		name: "this_is_the_chain_method".as_bytes().to_owned(),
-		arguments: "this_is_the_chain_arguments".as_bytes().to_owned(),
-	};
-	task_methods.push(task_method);
-	let onchain_task = TaskData {
-		task: ChainTask::SwapToken,
-		chain_data: "this_is_the_chain_data".as_bytes().to_owned(),
-		method: task_methods.clone(),
-	};
-	let onchain_task2 = TaskData {
-		task: ChainTask::SwapToken,
-		chain_data: "this_is_the_chain_data2".as_bytes().to_owned(),
-		method: task_methods.clone(),
-	};
-	task.push(onchain_task);
-	task2.push(onchain_task2);
-	let mut chain_task = Vec::new();
-	let mut chain_task2 = Vec::new();
-	let chain_tasks = 	OnchainTaskData{
-		task,
-	};
-	let chain_tasks2 = OnchainTaskData{
-		task: task2,
-	};
-	chain_task.push(chain_tasks.clone());
-	chain_task2.push(chain_tasks2.clone());
-
-	new_test_ext().execute_with(|| {
-		// Call the store task  extrinsic
-		assert_ok!(OnChainTask::edit_task_data(
-			RawOrigin::Signed(1).into(),
-			chain.clone(),
-			chain_tasks.clone(),
-			chain_tasks2.clone(),
-
-		));
-		// Retreiving the task stored via it's key and assert the result.
-		assert_eq!(OnChainTask::task_store(chain.clone()), Some(chain_task2.clone()));
 	});
 }
 

dcba6b77e3ae93a73466d5b48fe5b5249fd3d1c4
juniuszhou <junius.zhou@gmail.com>
2023-01-02 14:14:53 +0800

    fix unit test

--- pallets/tesseract-sig-storage/src/tests.rs
+++ pallets/tesseract-sig-storage/src/tests.rs
@@ -41,7 +41,7 @@ fn it_works_removing_tesseract_member() {
 #[test]
 fn test_signature_storage() {
 	let sig_data: SignatureData = "this_is_the_signature_data_1".as_bytes().to_owned();
-	let network_id = "12345".as_bytes();
+	let task_id = 1;
 	let block_height = 1;
 	new_test_ext().execute_with(|| {
 		// We first add the Tesseract as a member with root privilege
@@ -54,7 +54,7 @@ fn test_signature_storage() {
 		assert_ok!(TesseractSigStorage::store_signature(
 			RawOrigin::Signed(1).into(),
 			sig_data,
-			network_id.to_vec(),
+			task_id,
 			block_height
 		));
 

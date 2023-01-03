e832bfe7b403f4961b17fd90d540c600d1e47cc8
juniuszhou <junius.zhou@gmail.com>
2023-01-04 07:40:53 +0800

    fix a fmt error

--- workers/time-worker/src/worker.rs
+++ workers/time-worker/src/worker.rs
@@ -24,6 +24,7 @@ use time_primitives::{crypto::Signature, TimeApi, KEY_TYPE};
 use tss::{
 	local_state_struct::TSSLocalStateData,
 	tss_event_model::{TSSData, TSSEventType},
+	utils::get_reset_tss_msg,
 };
 
 /// Our structure, which holds refs to everything we need to operate

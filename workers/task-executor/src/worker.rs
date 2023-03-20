c44202248c146e47905e28f46c4bf8640926d5b6
ManojJiSharma <manoj.sharma.bit.india@gmail.com>
2023-03-20 22:13:05 +0530

    initial commit

--- workers/task-executor/src/worker.rs
+++ workers/task-executor/src/worker.rs
@@ -5,10 +5,14 @@ use sc_client_api::Backend;
 use sp_api::ProvideRuntimeApi;
 use sp_blockchain::Backend as SpBackend;
 use sp_runtime::{generic::BlockId, traits::Block};
+use std::error::Error;
 use std::{marker::PhantomData, sync::Arc, thread};
 use time_primitives::TimeApi;
 use time_worker::kv::TimeKeyvault;
 use tokio::{sync::Mutex, time};
+use web3::contract::{Contract, Options};
+use web3::types::{Address, U256};
+use dotenvy::dotenv;
 
 // use worker_aurora::{self, establish_connection, get_on_chain_data};
 
@@ -47,6 +51,36 @@ where
 		}
 	}
 
+	async fn call_contract_function(address:String, abi:String, method:String) -> Result<(), Box<dyn Error>> {
+		dotenv().ok();
+
+		let infura_url = std::env::var("INFURA_URL").expect("INFURA_URL must be set");
+
+		let websocket_result = web3::transports::WebSocket::new(&infura_url).await;
+		let websocket = match websocket_result {
+			Ok(websocket) => websocket,
+			Err(_) => web3::transports::WebSocket::new(&infura_url)
+				.await
+				.expect("Failed to create default websocket"),
+		};
+		let web3 = web3::Web3::new(websocket);
+
+		// Load the contract ABI and address
+		let contract_abi = abi.parse().unwrap();
+		let contract_address = Address::from(address.parse().unwrap());
+
+		// Create a new contract instance using the ABI and address
+		let contract = Contract::new(web3.eth(), contract_address, contract_abi);
+
+		// Call the "getGreeting" function on the contract instance
+		let greeting: String =
+			contract.query(method.as_str(), (), None, Options::default(), None).await?;
+
+		println!("The greeting is: {}", greeting);
+
+		Ok(())
+	}
+
 	pub(crate) async fn run(&mut self) {
 		let sign_data_sender_clone = self.sign_data_sender.clone();
 		let delay = time::Duration::from_secs(10);
@@ -55,9 +89,12 @@ where
 			if !keys.is_empty() {
 				let at = self.backend.blockchain().last_finalized().unwrap();
 				let at = BlockId::Hash(at);
-
+// MAP(id, t);
 				if let Ok(metadata) = self.runtime.runtime_api().get_task_metadata(&at) {
-					log::info!("New task metadata: {:?}", metadata);
+					log::info!("New task metadata: {:?}", metadata.unwrap());
+
+
+					// call_contract_function()
 				} else {
 					log::error!("Failed to get task metadata for block {:?}", at);
 				}

7f2a74dc2d07200ef30c30b1175a5e02a94f8199
1zain-haider1 <zainhaider350@gmail.com>
2023-03-29 15:22:11 +0500

    added shard id

--- client/nodejs/src/pallet_task_add.js
+++ client/nodejs/src/pallet_task_add.js
@@ -24,6 +24,7 @@ const pallet_task_add = async (_keyspair, who) => {
     const chan = new Channel(0 /* default */);
     const input_task = {
         collection_id: 11,
+        shard_id: 1,
         schema:[1],
         function:{ethereumcontract:{
             address: 'String',

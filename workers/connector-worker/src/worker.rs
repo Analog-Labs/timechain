1d40886b6f9e9443a693946b5a78b65efc735e4e
Haider-Ali-DS <haiderali.tech_786@outlook.com>
2023-04-03 15:44:07 +0500

    fixing variable name

--- workers/connector-worker/src/worker.rs
+++ workers/connector-worker/src/worker.rs
@@ -161,7 +161,7 @@ where
 				if let Err(e) = Self::get_latest_block_event(self, &client, &config).await {
 					log::error!("Error occured while fetching block data {e:?}");
 				}
-				tokio::time::sleep(duration).await;
+				tokio::time::sleep(delay).await;
 			}
 		}
 	}

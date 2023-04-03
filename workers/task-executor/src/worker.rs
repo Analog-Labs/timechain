8858996b24965aa6802eb660bbf9f41aa3aa4781
ManojJiSharma <manoj.sharma.bit.india@gmail.com>
2023-04-03 15:51:20 +0530

    code seprated in function

--- workers/task-executor/src/worker.rs
+++ workers/task-executor/src/worker.rs
@@ -19,8 +19,6 @@ use web3::{
 	types::H160,
 };
 
-// use worker_aurora::{self, establish_connection, get_on_chain_data};
-
 #[allow(unused)]
 /// Our structure, which holds refs to everything we need to operate
 pub struct TaskExecutor<B: Block, R, BE> {
@@ -117,6 +115,88 @@ where
 		Ok(())
 	}
 
+	async fn process_tasks_for_block(
+		&self,
+		block_id: BlockId<B>,
+		map: &mut HashMap<u64, String>,
+	) -> Result<(), Box<dyn std::error::Error>> {
+		// let tasks_schedule = self.runtime.runtime_api().get_task_schedule(&block_id)?;
+		// Get the task schedule for the current block
+		let tasks_schedule = match self.runtime.runtime_api().get_task_schedule(&block_id) {
+			Ok(task_schedule) => task_schedule,
+			Err(_e) => Ok({
+				log::error!("Failed to get task schedule for block {:?}", block_id);
+				Vec::new() // Return an empty vector as the default value
+			}),
+		};
+
+		match tasks_schedule {
+			Ok(task_schedule) => {
+				for schedule_task in task_schedule.iter() {
+					let task_id = schedule_task.task_id.0;
+					let shard_id = schedule_task.shard_id;
+
+					match map.insert(task_id, "hash".to_string()) {
+						Some(old_value) =>
+							println!("The key already existed with the value {}", old_value),
+						None => {
+							println!("The key didn't exist and was inserted key {}.", task_id);
+							let metadata_result = self
+								.runtime
+								.runtime_api()
+								.get_task_metadat_by_key(&block_id, task_id)?;
+
+							match metadata_result {
+								Ok(metadata) => {
+									for task in metadata.iter() {
+										match &task.function {
+											// If the task function is an Ethereum contract call,
+											// call it and send for signing
+											Function::EthereumContract {
+												address,
+												abi,
+												function,
+												input: _,
+												output: _,
+											} => {
+												let _result =
+													Self::call_contract_and_send_for_sign(
+														self,
+														address.to_string(),
+														abi.to_string(),
+														function.to_string(),
+														shard_id,
+													)
+													.await;
+											},
+											Function::EthereumApi {
+												function: _,
+												input: _,
+												output: _,
+											} => {
+												todo!()
+											},
+										};
+									}
+								},
+								Err(e) => {
+									log::warn!(
+										"Failed to get task metadata for block {:?} {:?}",
+										block_id,
+										e
+									);
+								},
+							}
+						},
+					}
+				}
+			},
+			Err(e) => log::warn!("getting error on task schedule {:?}", e),
+		}
+
+		Ok(())
+	}
+
 	pub(crate) async fn run(&mut self) {
 		// Set the delay for the loop
 		let delay = time::Duration::from_secs(10);
@@ -128,88 +208,10 @@ where
 				// Get the last finalized block from the blockchain
 				if let Ok(at) = self.backend.blockchain().last_finalized() {
 					let at = BlockId::Hash(at);
-
-					// Get the task schedule for the current block
-					let tasks_schedule = match self.runtime.runtime_api().get_task_schedule(&at) {
-						Ok(task_schedule) => task_schedule,
-						Err(_e) => Ok({
-							log::error!("Failed to get task schedule for block {:?}", at);
-							Vec::new() // Return an empty vector as the default value
-						}),
-					};
-					match tasks_schedule {
-						Ok(task_schedule) =>
-							for schedule_task in task_schedule.iter() {
-								let task_id = schedule_task.task_id.0;
-								let shard_id = schedule_task.shard_id;
-
-								match map.insert(task_id, "hash".to_string()) {
-									Some(old_value) => println!(
-										"The key already existed with the value {}",
-										old_value
-									),
-									None => {
-										println!(
-											"The key didn't exist and was inserted key {}.",
-											task_id
-										);
-										if let Ok(metadata_result) = self
-											.runtime
-											.runtime_api()
-											.get_task_metadat_by_key(&at, task_id)
-										{
-											match metadata_result {
-												Ok(metadata) =>
-													for task in metadata.iter() {
-														match &task.function {
-															// If the task function is an Ethereum
-															// contract call, call it and send for
-															// signing
-															Function::EthereumContract {
-																address,
-																abi,
-																function,
-																input: _,
-																output: _,
-															} => {
-																let _result =
-																	Self::call_contract_and_send_for_sign(
-																		self,
-																		address.to_string(),
-																		abi.to_string(),
-																		function.to_string(),
-																		shard_id,
-																	)
-																	.await;
-															},
-															Function::EthereumApi {
-																function: _,
-																input: _,
-																output: _,
-															} => {
-																todo!()
-															},
-														};
-													},
-												Err(e) => {
-													log::info!(
-														"No metadata found for block {:?}: {:?}",
-														at,
-														e
-													);
-												},
-											}
-										} else {
-											log::error!(
-												"Failed to get task metadata for block {:?}",
-												at
-											);
-										}
-									},
-								}
-							},
+					match self.process_tasks_for_block(at, &mut map).await {
+						Ok(_) => (),
 						Err(e) => {
-							log::info!("No metadata found for block {:?}: {:?}", at, e);
+							log::error!("Failed to process tasks for block {:?}: {:?}", at, e);
 						},
 					}
 				} else {

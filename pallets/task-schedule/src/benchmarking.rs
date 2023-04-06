48f6b4b272d703e08091e980b2943721348a1aae
1zain-haider1 <zainhaider350@gmail.com>
2023-04-06 18:05:35 +0500

    updated tests

--- pallets/task-schedule/src/benchmarking.rs
+++ pallets/task-schedule/src/benchmarking.rs
@@ -4,20 +4,18 @@ use crate::Pallet as TaskSchedule;
 use frame_benchmarking::{benchmarks, whitelisted_caller};
 use frame_system::RawOrigin;
 use scale_info::prelude::string::String;
-use time_primitives::abstraction::{ObjectId, ScheduleStatus, TaskSchedule as Schedule, Validity};
+use time_primitives::abstraction::{ObjectId, ScheduleInput as Schedule, ScheduleStatus, Validity};
 
 benchmarks! {
 
 	insert_schedule {
 		let origin: T::AccountId = whitelisted_caller();
-		let input: Schedule = Schedule {
+		let input  = Schedule {
 			task_id: ObjectId(1),
-			owner: origin,
 			shard_id: 1,
 			cycle: 12,
 			validity: Validity::Seconds(10),
 			hash:String::from("address"),
-			status: ScheduleStatus::Initiated
 		};
 
 		let schedule = input.clone();

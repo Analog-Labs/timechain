a24cc1e3510c6016019088e285f6140832f30fff
ManojJiSharma <manoj.sharma.bit.india@gmail.com>
2023-05-08 11:13:10 +0530

    removed latest migration

--- src/schema.rs
+++ src/schema.rs
@@ -1,31 +1,6 @@
 // @generated automatically by Diesel CLI.
 
 diesel::table! {
-    _collections_ (name) {
-        name -> Text,
-        cid -> Int8,
-    }
-}
-
-diesel::table! {
-    _feeds_ (id) {
-        id -> Int8,
-        hash -> Bpchar,
-        task -> Bytea,
-        validity -> Int8,
-        timestamp -> Nullable<Timestamp>,
-        cycle -> Nullable<Int8>,
-    }
-}
-
-diesel::table! {
-    _ql_ (name) {
-        name -> Text,
-        elements -> Nullable<Text>,
-    }
-}
-
-diesel::table! {
     chains (chain_id) {
         chain_id -> Int4,
         chain_name -> Varchar,
@@ -62,26 +37,13 @@ diesel::table! {
     }
 }
 
-diesel::table! {
-    the_last_cycle (id) {
-        id -> Int4,
-        cycle -> Nullable<Int8>,
-        updated -> Nullable<Timestamp>,
-    }
-}
-
-diesel::joinable!(_collections_ -> _feeds_ (cid));
 diesel::joinable!(on_chain_data -> tasks (task_id));
 diesel::joinable!(tasks -> chains (chain_id));
 diesel::joinable!(tasks -> task_metadata (task_metadata_id));
 
 diesel::allow_tables_to_appear_in_same_query!(
-    _collections_,
-    _feeds_,
-    _ql_,
     chains,
     on_chain_data,
     task_metadata,
     tasks,
-    the_last_cycle,
 );

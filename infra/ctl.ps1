956abe0b72074f4be0d50800cdf953671118dcb3
Alexey Sudachen <alexey@sudachen.name>
2023-05-11 04:47:12 -0400

    updated scripts

--- infra/ctl.ps1
+++ infra/ctl.ps1
@@ -1,25 +1,45 @@
 #!/usr/bin/env pwsh
-Param([switch]$Up,[switch]$Down,[switch]$Build,[switch]$Upload,[switch]$Cargo)
+Param(
+    [switch]$Up,
+    [switch]$Down,
+    [switch]$Build,
+    [switch]$Upload,
+    [switch]$Cargo,
+    [switch]$Restart)
+
 Push-Location $PSScriptRoot
 
-if ( $Cargo -or -not(Test-path ../cargo.toml.tar -PathType leaf)) {
+if ($Cargo -or -not(Test-path ../cargo.toml.tar -PathType leaf))
+{
     ./make.cargo.tar.sh
 }
 
-if ( $Build ) {
+if ($Build -or $Upload)
+{
     docker build .. -f ./Dockerfile -t ghcr.io/analog-labs/testnet
 }
 
-if ( $Upload ) {
+if ($Upload)
+{
     docker push ghcr.io/analog-labs/testnet
 }
 
-if ( $Down -or $Up ) {
+if ($Down -or $Up)
+{
     docker compose down -v
 }
 
-if ( $Up ) {
+if ($Up)
+{
     docker compose up -V --force-recreate -d
+}
+
+if ($Restart)
+{
+    Push-Location aws
+    terraform apply -destroy -target aws_instance.validator_node -target aws_instance.boot_node -auto-approve
+    terraform apply -auto-approve
+    Pop-Location
 }
 
 Pop-Location

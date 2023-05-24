ab477f3b147c1109eb52f887b121f542557ecf0b
juniuszhou <junius.zhou@gmail.com>
2023-05-24 18:45:40 +0800

    add macos in docker

--- build_docker.sh
+++ build_docker.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
-cargo build -p timechain-node --target x86_64-unknown-linux-musl --release
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    TARGET_CC=x86_64-linux-musl-gcc cargo build -p timechain-node --release
+else
+    cargo build -p timechain-node --target x86_64-unknown-linux-musl --release
+fi
 mkdir -p target/release/timechain-node/bin
 mv target/x86_64-unknown-linux-musl/release/timechain-node target/release/timechain-node/bin
 docker build target/release/timechain-node -f Dockerfile -t analoglabs/timechain-node

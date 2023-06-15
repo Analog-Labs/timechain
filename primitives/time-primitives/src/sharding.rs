f462cebe65554153920ff91fa4a181282682bf06
4meta5 <asinghchrony@protonmail.com>
2023-06-15 14:12:14 -0400

    init

--- primitives/time-primitives/src/sharding.rs
+++ primitives/time-primitives/src/sharding.rs
@@ -43,6 +43,10 @@ impl Shard {
 		}
 	}
 
+	pub fn is_collector(&self, id: &TimeId) -> bool {
+		self.collector() == id
+	}
+
 	/// Sets collector to a given ID if is not the same.
 	/// Returns error if `collector` is not member of this shard.
 	/// # Param
@@ -69,6 +73,15 @@ impl Shard {
 			Shard::Five(set) => set.to_vec(),
 			Shard::Ten(set) => set.to_vec(),
 		}
+	}
+
+	pub fn contains_member(&self, member: &TimeId) -> bool {
+		let members = match self {
+			Shard::Three(s) => s.to_vec(),
+			Shard::Five(s) => s.to_vec(),
+			Shard::Ten(s) => s.to_vec(),
+		};
+		members.contains(member)
 	}
 
 	/// Returns the shard threshold.

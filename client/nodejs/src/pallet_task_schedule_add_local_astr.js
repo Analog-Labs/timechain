c9aba018e4c5d2ad69b48ecba3dcc072c8febbc0
Haider-Ali-DS <haiderali.tech_786@outlook.com>
2023-07-10 23:09:26 +0500

    resetting script params

--- client/nodejs/src/pallet_task_schedule_add_local_astr.js
+++ client/nodejs/src/pallet_task_schedule_add_local_astr.js
@@ -25,9 +25,9 @@ const pallet_task_add = async (_keyspair, who) => {
     const input_task = {
         task_id: 2,
         owner: 'address',
-        shard_id: 0,
+        shard_id: 1,
         frequency: 10,
-        cycle: 2,
+        cycle: 3,
         validity: { Seconds: 12 },
         hash: 'asdasd',
         status: 0

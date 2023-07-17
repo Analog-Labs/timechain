45c0b1d0ccc7fee12acab25c7185d08efb3c5267
Lohann Paterno Coutinho Ferreira <lohann@enjin.io>
2023-07-17 15:32:39 -0300

    support arm64 targets

--- README.md
+++ README.md
@@ -31,11 +31,11 @@ This repo includes the minimum required components to start a timechain testnet 
 
 Follow the steps below to get started.
 
-### Rust Setup
+### 1. Rust Setup
 
 First, complete the [Dev Docs Installation](https://docs.substrate.io/v3/getting-started/installation/).
 
-### Build and Run
+### 1.1 Build and Run
 
 Use the following command to build the node and run it after build successfully:
 
@@ -46,20 +46,30 @@ cargo build --release
 
 ## Run Using Docker
 
-if you're on macos add the following to your `.cargo/config`:
+### MacOS
+if you're on macos, install [musl-cross](https://github.com/FiloSottile/homebrew-musl-cross) for enable musl-target cross-compilation:
+```bash
+brew install filosottile/musl-cross/musl-cross --with-aarch64
 ```
+
+add the following to your `.cargo/config`:
+```toml
 [target.x86_64-unknown-linux-musl]
 linker = "x86_64-linux-musl-gcc"
 
+[target.aarch64-unknown-linux-musl]
+linker = "aarch64-linux-musl-gcc"
+
 [env]
 CC_x86_64-unknown-linux-musl = "x86_64-linux-musl-gcc"
+CC_aarch64-unknown-linux-musl = "aarch64-linux-musl-gcc"
 ```
 
+### Shared Steps
 1. Install docker, docker compose and musl using your system package manager.
-2. Install the musl target with rustup `rustup target add x86_64-unknown-linux-musl`.
-3. Build the docker image with `./build_docker.sh`.
-4. Start the timechain validators, connectors and chain nodes with `docker compose up`.
-5. Set the validator keys with `./set_keys.sh`.
+2. Build the docker image with `./build_docker.sh`.
+3. Start the timechain validators, connectors and chain nodes with `docker compose up -d`.
+4. Set the validator keys with `./set_keys.sh`.
 
 ## Run public testnet
 
--- build_docker.sh
+++ build_docker.sh
@@ -1,5 +1,43 @@
 #!/bin/sh
-cargo build -p timechain-node --target x86_64-unknown-linux-musl --release
+
+set -e
+
+# Check for 'uname' and abort if it is not available.
+uname -v > /dev/null 2>&1 || { echo >&2 "ERROR - requires 'uname' to identify the platform."; exit 1; }
+
+# Check for 'docker' and abort if it is not running.
+docker info > /dev/null 2>&1 || { echo >&2 "ERROR - requires 'docker', please start docker and try again."; exit 1; }
+
+# Check for 'rustup' and abort if it is not available.
+rustup -V > /dev/null 2>&1 || { echo >&2 "ERROR - requires 'rustup' for compile the binaries"; exit 1; }
+
+# Detect host architecture
+case "$(uname -m)" in
+    x86_64)
+        rustTarget='x86_64-unknown-linux-musl'
+        muslLinker='x86_64-linux-musl-gcc'
+        ;;
+    arm64|aarch64)
+        rustTarget='aarch64-unknown-linux-musl'
+        muslLinker='aarch64-linux-musl-gcc'
+        ;;
+    *)
+        echo >&2 "ERROR - unsupported architecture: $(uname -m)"
+        exit 1
+        ;;
+esac
+
+# Check if the musl linker is installed
+"$muslLinker" --version > /dev/null 2>&1 || { echo >&2 "ERROR - requires '$muslLinker' linker for compile"; exit 1; }
+
+# Check if the rust target is installed
+if ! rustup target list | grep -q "$rustTarget"; then
+  echo "Installing the musl target with rustup '$rustTarget'"
+  rustup target add "$rustTarget"
+fi
+
+# Build docker image
+cargo build -p timechain-node --target "$rustTarget" --release
 mkdir -p target/release/timechain-node/bin
-mv target/x86_64-unknown-linux-musl/release/timechain-node target/release/timechain-node/bin
+mv "target/$rustTarget/release/timechain-node" target/release/timechain-node/bin
 docker build target/release/timechain-node -f docker/Dockerfile -t analoglabs/timechain-node

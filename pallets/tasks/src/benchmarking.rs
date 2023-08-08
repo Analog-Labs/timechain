f6108cc7bd57ff0c00decde6cb9d5712f22313b6
4meta5 <asinghchrony@protonmail.com>
2023-08-08 00:23:33 -0400

    clean

--- pallets/tasks/src/benchmarking.rs
+++ pallets/tasks/src/benchmarking.rs
@@ -4,22 +4,18 @@ use frame_system::RawOrigin;
 use time_primitives::{Function, Network, ScheduleInput};
 
 benchmarks! {
-	create_task {
-		let origin: T::AccountId = whitelisted_caller();
-		let schedule  = ScheduleInput {
-			network: Network::Ethereum,
-			function: Function::EVMViewWithoutAbi {
-				address: Default::default(),
-				function_signature: Default::default(),
-				input: Default::default(),
-			},
-			cycle: 1,
-			start: 0,
-			period: 1,
-			hash: "".to_string(),
-		};
-	}: _(RawOrigin::Signed(origin), schedule)
-	verify {}
+	create_task {}: _(RawOrigin::Signed(whitelisted_caller()), ScheduleInput {
+		network: Network::Ethereum,
+		function: Function::EVMViewWithoutAbi {
+			address: Default::default(),
+			function_signature: Default::default(),
+			input: Default::default(),
+		},
+		cycle: 1,
+		start: 0,
+		period: 1,
+		hash: "".to_string(),
+	}) verify {}
 
 	impl_benchmark_test_suite!(Pallet, crate::mock::new_test_ext(), crate::mock::Test);
 }

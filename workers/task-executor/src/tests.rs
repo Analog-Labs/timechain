5bba6169c2736ead8099cef6facbd7f14ea0980d
Haider-Ali-DS <haiderali.tech_786@outlook.com>
2023-08-15 21:13:07 +0500

    initialized task_executor in test

--- workers/task-executor/src/tests.rs
+++ workers/task-executor/src/tests.rs
@@ -1,7 +1,11 @@
+use crate::{TaskExecutor, TaskExecutorParams};
 use anyhow::Result;
-use sc_network_test::Block;
+use sc_network_test::{Block, TestClientBuilder, TestClientBuilderExt};
 use sp_api::{ApiRef, ProvideRuntimeApi};
 use time_primitives::{PeerId, ShardId, TaskDescriptor, TaskExecution, TaskId, TimeApi};
+use std::marker::PhantomData;
+use std::sync::Arc;
+use futures::channel::mpsc;
 
 #[derive(Clone, Default)]
 struct MockApi;
@@ -33,5 +37,34 @@ impl ProvideRuntimeApi<Block> for MockApi {
 
 #[tokio::test]
 async fn task_executor_smoke() -> Result<()> {
+	let (sign_data_sender, _sign_data_receiver) = mpsc::channel(400);
+
+	let (client, backend) = {
+		let builder = TestClientBuilder::with_default_backend();
+		let backend = builder.backend();
+		let (client, _) = builder.build_with_longest_chain();
+		(Arc::new(client), backend)
+	};
+
+	let api = Arc::new(MockApi::default());
+
+	let task_spawner = futures::executor::block_on(crate::Task::new(crate::TaskSpawnerParams {
+		tss: sign_data_sender,
+		connector_url: Some("".into()),
+		connector_blockchain: Some("".into()),
+		connector_network: Some("".into()),
+	}))
+	.unwrap();
+
+	let params = TaskExecutorParams {
+		_block: PhantomData::default(),
+		backend,
+		client,
+		runtime: api,
+		peer_id: [0u8; 32],
+		task_spawner,
+	};
+
+	let _task_executor = TaskExecutor::new(params);
 	Ok(())
 }

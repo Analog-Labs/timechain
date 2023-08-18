0d1fc290e305e6073e345724d78795dd590f11ca
Haider-Ali-DS <haiderali.tech_786@outlook.com>
2023-08-18 05:30:04 +0500

    basic setup

--- js/src/add_task_eth.js
+++ js/src/add_task_eth.js
@@ -1,6 +1,6 @@
 import { ApiPromise, WsProvider, Keyring } from '@polkadot/api';
 import { Channel } from 'async-channel';
-import { dirname} from 'path';
+import { dirname } from 'path';
 import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -12,20 +12,18 @@ const stringToHex = (str) => {
 
 const setup_substrate = async () => {
     const wsProvider = new WsProvider(node_address);
-    // const custom_types = await get_custom_types();
     const api = await ApiPromise.create({
-        provider: wsProvider
+        provider: wsProvider,
+        noInitWarn: true
     });
-    //         , "types": custom_types
     return api;
 };
 
 const pallet_task_add = async (_keyspair, who) => {
-    const api =  await setup_substrate();
+    const api = await setup_substrate();
     const keyring = new Keyring({ type: 'sr25519' });
     const keyspair = keyring.addFromUri('//Alice', { name: 'Alice default' });
-
-    const chan = new Channel(0 /* default */);
+    
     const input_task = {
         network: 0,
         cycle: 4,
@@ -36,17 +34,20 @@ const pallet_task_add = async (_keyspair, who) => {
             EVMViewWithoutAbi: {
                 address: stringToHex('0x3de7086ce750513ef79d14eacbd1282c4e4b0cea'),
                 function_signature: "function get_votes_stats() external view returns (uint[] memory)",
-                input: 2,
+                input: [],
             }
         },
     }
     await api.isReady;
-    console.log("api.tx.task_meta ---> ", api.tx.taskSchedule);
-    const unsub = await api.tx.tasks.createTask(input_task).signAndSend(keyspair, ({ status, events, dispatchError }) => {
-        console.log(`Current status is ${status}`);
+    const result = await api.tx.tasks.createTask(input_task).signAndSend(keyspair, ({ status, events }) => {
+        if (status.isInBlock || status.isFinalized) {
+            const filtered_events = events.filter(({ event }) => api.events.tasks.TaskCreated.is(event))
+            if (filtered_events.length >= 1) {
+                console.log("registered_task:", filtered_events[0].event.data.toJSON().pop());
+                process.exit(0);
+            }
+        }
     });
-    
-    await chan.get().then(value => console.log(value), error => console.error(error));
 };
 
 pallet_task_add();

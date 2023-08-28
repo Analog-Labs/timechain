bb93215d270a30f524a76c8fb15aabe11dd9924a
4meta5 <asinghchrony@protonmail.com>
2023-08-28 19:36:14 -0400

    update shards tests to remove PeerId as well

--- pallets/shards/src/tests.rs
+++ pallets/shards/src/tests.rs
@@ -2,14 +2,24 @@ use crate::mock::*;
 use crate::{Error, Event, ShardMembers, ShardNetwork, ShardState};
 use frame_support::{assert_noop, assert_ok};
 use frame_system::RawOrigin;
-use time_primitives::{Network, OcwShardInterface, PeerId, PublicKey};
+use time_primitives::{AccountId, Network, OcwShardInterface, PublicKey};
 
-const A: PeerId = [1u8; 32];
-const B: PeerId = [2u8; 32];
-const C: PeerId = [3u8; 32];
-const D: PeerId = [4u8; 32];
-const E: PeerId = [5u8; 32];
-const F: PeerId = [6u8; 32];
+fn shards() -> [[AccountId; 3]; 3] {
+	let a: AccountId = [1u8; 32].into();
+	let b: AccountId = [2u8; 32].into();
+	let c: AccountId = [3u8; 32].into();
+	let d: AccountId = [4u8; 32].into();
+	let e: AccountId = [5u8; 32].into();
+	let f: AccountId = [6u8; 32].into();
+	[[a.clone(), b.clone(), c.clone()], [c, b, a], [d, e, f]]
+}
+
+fn first_shard() -> [AccountId; 3] {
+	let a: AccountId = [1u8; 32].into();
+	let b: AccountId = [2u8; 32].into();
+	let c: AccountId = [3u8; 32].into();
+	[a, b, c]
+}
 
 fn collector() -> PublicKey {
 	PublicKey::Sr25519(sp_core::sr25519::Public::from_raw([42; 32]))
@@ -17,7 +27,7 @@ fn collector() -> PublicKey {
 
 #[test]
 fn test_register_shard() {
-	let shards = [[A, B, C], [C, B, A], [D, E, F]];
+	let shards = shards();
 	new_test_ext().execute_with(|| {
 		for shard in &shards {
 			assert_ok!(Shards::register_shard(
@@ -34,7 +44,7 @@ fn test_register_shard() {
 			assert_eq!(members.len(), shard.len());
 			assert_eq!(threshold, 1);
 		}
-		for member in [A, B, C] {
+		for member in first_shard() {
 			let shards = Shards::get_shards(member);
 			assert_eq!(shards.len(), 2);
 		}
@@ -46,7 +56,7 @@ fn test_register_shard() {
 
 #[test]
 fn cannot_submit_public_key_if_shard_not_exists() {
-	let shards = [[A, B, C], [C, B, A], [D, E, F]];
+	let shards = shards();
 	new_test_ext().execute_with(|| {
 		for (shard_id, _) in shards.iter().enumerate() {
 			assert_noop!(
@@ -59,7 +69,7 @@ fn cannot_submit_public_key_if_shard_not_exists() {
 
 #[test]
 fn submit_public_key_max_once() {
-	let shards = [[A, B, C], [C, B, A], [D, E, F]];
+	let shards = shards();
 	new_test_ext().execute_with(|| {
 		for shard in &shards {
 			assert_ok!(Shards::register_shard(
@@ -82,7 +92,7 @@ fn submit_public_key_max_once() {
 
 #[test]
 fn test_set_shard_offline() {
-	let shards = [[A, B, C], [C, B, A], [D, E, F]];
+	let shards = shards();
 	new_test_ext().execute_with(|| {
 		for shard in &shards {
 			assert_ok!(Shards::register_shard(
@@ -102,7 +112,7 @@ fn test_set_shard_offline() {
 
 #[test]
 fn cannot_set_shard_offline_if_no_shard() {
-	let shards = [[A, B, C], [C, B, A], [D, E, F]];
+	let shards = shards();
 	new_test_ext().execute_with(|| {
 		for (shard_id, _) in shards.iter().enumerate() {
 			assert_noop!(Shards::set_shard_offline(shard_id as _), Error::<Test>::UnknownShard);
@@ -112,7 +122,7 @@ fn cannot_set_shard_offline_if_no_shard() {
 
 #[test]
 fn offline_shard_cannot_be_set_offline() {
-	let shards = [[A, B, C], [C, B, A], [D, E, F]];
+	let shards = shards();
 	new_test_ext().execute_with(|| {
 		for shard in &shards {
 			assert_ok!(Shards::register_shard(
@@ -140,7 +150,7 @@ fn dkg_times_out() {
 		assert_ok!(Shards::register_shard(
 			RawOrigin::Root.into(),
 			Network::Ethereum,
-			[A, B, C].to_vec(),
+			first_shard().to_vec(),
 			collector(),
 			1,
 		));

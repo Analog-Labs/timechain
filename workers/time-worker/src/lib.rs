20f012ae42ec688ee7e9b41091178c6d3785e62a
David Craven <david@craven.ch>
2023-09-02 23:16:57 +0200

    Refactor chronicle.

--- workers/time-worker/src/lib.rs
+++ workers/time-worker/src/lib.rs
@@ -16,6 +16,8 @@ use time_primitives::{
 	MembersApi, PeerId, PublicKey, ShardsApi, SubmitMembers, SubmitShards, TaskExecutor, TssRequest,
 };
 
+pub use worker::{TimeWorker, TimeWorkerParams};
+
 /// Constant to indicate target for logging
 pub const TW_LOG: &str = "time-worker";
 
@@ -31,69 +33,4 @@ pub fn protocol_config(tx: async_channel::Sender<IncomingRequest>) -> RequestRes
 		request_timeout: Duration::from_secs(3),
 		inbound_queue: Some(tx),
 	}
-}
-
-/// Set of properties we need to run our gadget
-pub struct TimeWorkerParams<B: Block, C, R, N, T, TxSub>
-where
-	B: Block + 'static,
-	C: BlockchainEvents<B> + HeaderBackend<B> + 'static,
-	R: ProvideRuntimeApi<B> + 'static,
-	R::Api: MembersApi<B> + ShardsApi<B>,
-	N: NetworkRequest,
-	T: TaskExecutor<B>,
-	TxSub: SubmitShards<B> + SubmitMembers<B>,
-{
-	pub _block: PhantomData<B>,
-	pub client: Arc<C>,
-	pub runtime: Arc<R>,
-	pub network: N,
-	pub task_executor: T,
-	pub tx_submitter: TxSub,
-	pub public_key: PublicKey,
-	pub peer_id: PeerId,
-	pub tss_request: mpsc::Receiver<TssRequest>,
-	pub protocol_request: async_channel::Receiver<IncomingRequest>,
-}
-
-/// Start the Timeworker gadget.
-///
-/// This is a thin shim around running and awaiting a time worker.
-pub async fn start_timeworker_gadget<B, C, R, N, T, TxSub>(
-	timeworker_params: TimeWorkerParams<B, C, R, N, T, TxSub>,
-) where
-	B: Block + 'static,
-	C: BlockchainEvents<B> + HeaderBackend<B> + 'static,
-	R: ProvideRuntimeApi<B> + 'static,
-	R::Api: MembersApi<B> + ShardsApi<B>,
-	N: NetworkRequest,
-	T: TaskExecutor<B>,
-	TxSub: SubmitShards<B> + SubmitMembers<B>,
-{
-	let TimeWorkerParams {
-		_block,
-		client,
-		runtime,
-		network,
-		task_executor,
-		tx_submitter,
-		public_key,
-		peer_id,
-		tss_request,
-		protocol_request,
-	} = timeworker_params;
-	let worker_params = worker::WorkerParams {
-		_block,
-		client,
-		runtime,
-		network,
-		task_executor,
-		tx_submitter,
-		public_key,
-		peer_id,
-		tss_request,
-		protocol_request,
-	};
-	let mut worker = worker::TimeWorker::new(worker_params);
-	worker.run().await
 }

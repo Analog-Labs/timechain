5044177ed8d966eadbb6bd14f69a0f63c7507408
David Craven <david@craven.ch>
2023-09-06 15:10:02 +0200

    Fix benchmarks.

--- pallets/shards/src/benchmarking.rs
+++ pallets/shards/src/benchmarking.rs
@@ -2,10 +2,17 @@ use super::*;
 use crate::Pallet;
 use frame_benchmarking::benchmarks;
 use frame_system::RawOrigin;
+use sp_std::vec;
+
+pub const ALICE: [u8; 32] = [1u8; 32];
 
 benchmarks! {
-	submit_tss_public_key {
-	}: _(RawOrigin::None, 1, [0; 33])
+	commit {
+	}: _(RawOrigin::Signed(ALICE.into()), 1, vec![], [0; 65])
+	verify { }
+
+	ready {
+	}: _(RawOrigin::Signed(ALICE.into()), 1)
 	verify { }
 
 	impl_benchmark_test_suite!(Pallet, crate::mock::new_test_ext(), crate::mock::Test);

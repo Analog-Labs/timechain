0ea92e29feb8aa58efb70d5b17c52819a32bcf02
David Craven <david@craven.ch>
2023-09-07 21:12:57 +0200

    More fixes.

--- scripts/pull_images.sh
+++ scripts/pull_images.sh
@@ -1,5 +1,3 @@
 #!/bin/sh
 docker image pull ethereum/client-go:v1.10.26
 docker image pull staketechnologies/astar-collator:latest
-docker image pull analoglabs/connector-ethereum:latest
-docker image pull analoglabs/connector-astar:latest

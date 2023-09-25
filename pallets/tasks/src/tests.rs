545e594deef8cb9eb50f53f3bd0f1b0ceff0c997
Haider-Ali-DS <haiderali.tech_786@outlook.com>
2023-09-25 23:33:37 +0500

    syntax fix

--- pallets/tasks/src/tests.rs
+++ pallets/tasks/src/tests.rs
@@ -70,7 +70,12 @@ fn test_create_task() {
 			Tasks::get_shard_tasks(1),
 			vec![TaskExecution::new(0, 0, 0, TaskPhase::default())]
 		);
-		assert_ok!(Tasks::submit_resultRawOrigin::Signed([0; 32].into()).into(), 0, 0, mock_result_ok(1)));
+		assert_ok!(Tasks::submit_result(
+			RawOrigin::Signed([0; 32].into()).into(),
+			0,
+			0,
+			mock_result_ok(1)
+		));
 		System::assert_last_event(Event::<Test>::TaskResult(0, 0, mock_result_ok(1)).into());
 	});
 }
@@ -245,7 +250,12 @@ fn submit_completed_result_purges_task_from_storage() {
 			RawOrigin::Signed([0; 32].into()).into(),
 			mock_task(Network::Ethereum, 1)
 		));
-		assert_ok!(Tasks::submit_result(RawOrigin::Signed([0; 32].into()).into(), 0, 0, mock_result_ok(1)));
+		assert_ok!(Tasks::submit_result(
+			RawOrigin::Signed([0; 32].into()).into(),
+			0,
+			0,
+			mock_result_ok(1)
+		));
 		assert!(ShardTasks::<Test>::iter().collect::<Vec<_>>().is_empty());
 		assert!(UnassignedTasks::<Test>::iter().collect::<Vec<_>>().is_empty());
 	});
@@ -291,7 +301,12 @@ fn submit_task_error_increments_retry_count() {
 			signature: [0; 64],
 		};
 		for _ in 1..=10 {
-			assert_ok!(Tasks::submit_error(RawOrigin::Signed([0; 32].into()).into(), 0, 0, error.clone()));
+			assert_ok!(Tasks::submit_error(
+				RawOrigin::Signed([0; 32].into()).into(),
+				0,
+				0,
+				error.clone()
+			));
 		}
 		assert_eq!(TaskRetryCounter::<Test>::get(0), 10);
 	});
@@ -311,7 +326,12 @@ fn submit_task_error_over_max_retry_count_is_task_failure() {
 			signature: [0; 64],
 		};
 		for _ in 1..4 {
-			assert_ok!(Tasks::submit_error(RawOrigin::Signed([0; 32].into()).into(), 0, 0, error.clone()));
+			assert_ok!(Tasks::submit_error(
+				RawOrigin::Signed([0; 32].into()).into(),
+				0,
+				0,
+				error.clone()
+			));
 		}
 		System::assert_last_event(Event::<Test>::TaskFailed(0, 0, error).into());
 	});
@@ -331,10 +351,20 @@ fn submit_task_result_resets_retry_count() {
 			signature: [0; 64],
 		};
 		for _ in 1..=10 {
-			assert_ok!(Tasks::submit_error(RawOrigin::Signed([0; 32].into()).into(), 0, 0, error.clone()));
+			assert_ok!(Tasks::submit_error(
+				RawOrigin::Signed([0; 32].into()).into(),
+				0,
+				0,
+				error.clone()
+			));
 		}
 		assert_eq!(TaskRetryCounter::<Test>::get(0), 10);
-		assert_ok!(Tasks::submit_result(RawOrigin::Signed([0; 32].into()).into(), 0, 0, mock_result_ok(1)));
+		assert_ok!(Tasks::submit_result(
+			RawOrigin::Signed([0; 32].into()).into(),
+			0,
+			0,
+			mock_result_ok(1)
+		));
 		assert_eq!(TaskRetryCounter::<Test>::get(0), 0);
 	});
 }
@@ -561,7 +591,12 @@ fn submit_task_result_inserts_at_input_cycle() {
 			mock_task(Network::Ethereum, 5)
 		));
 		Tasks::shard_online(1, Network::Ethereum);
-		assert_ok!(Tasks::submit_result(RawOrigin::Signed([0; 32].into()).into(), 0, 0, mock_result_ok(1)));
+		assert_ok!(Tasks::submit_result(
+			RawOrigin::Signed([0; 32].into()).into(),
+			0,
+			0,
+			mock_result_ok(1)
+		));
 		assert_eq!(TaskCycleState::<Test>::get(0), 1);
 		assert!(TaskResults::<Test>::get(0, 0).is_some());
 		assert!(TaskResults::<Test>::get(0, 1).is_none());
@@ -615,7 +650,12 @@ fn resume_failed_task_after_shard_offline() {
 		Tasks::shard_online(1, Network::Ethereum);
 		// fails 3 time to turn task status to failed
 		for _ in 0..3 {
-			assert_ok!(Tasks::submit_error(RawOrigin::Signed([0; 32].into()).into(), 0, 0, mock_error.clone()));
+			assert_ok!(Tasks::submit_error(
+				RawOrigin::Signed([0; 32].into()).into(),
+				0,
+				0,
+				mock_error.clone()
+			));
 		}
 		assert_eq!(Tasks::task_shard(0), Some(1));
 		assert_eq!(Tasks::task_state(0), Some(TaskStatus::Failed { error: mock_error }));

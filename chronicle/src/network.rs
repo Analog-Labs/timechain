fad1e4a92a0d98763f469c2a26726c2236c2aa45
Haider-Ali-DS <haiderali.tech_786@outlook.com>
2023-09-26 20:02:56 +0500

    fix node crash on tx pool unwrap

--- chronicle/src/network.rs
+++ chronicle/src/network.rs
@@ -24,8 +24,9 @@ use time_primitives::{
 	BlockTimeApi, MembersApi, PublicKey, ShardId, ShardStatus, ShardsApi, SubmitMembers,
 	SubmitShards, TaskExecutor, TssId, TssSignature, TssSigningRequest,
 };
-use tokio::time::{interval_at, Duration, Instant};
+use tokio::time::{interval_at, sleep, Duration, Instant};
 use tracing::{event, span, Level, Span};
+
 use tss::{SigningKey, TssAction, TssMessage, VerifiableSecretSharingCommitment, VerifyingKey};
 
 #[derive(Deserialize, Serialize)]
@@ -450,21 +451,26 @@ where
 		}));
 	}
 
-	pub async fn run(mut self, span: &Span) {
-		event!(
+    pub async fn run(mut self, span: &Span) {
+        event!(
 			target: TW_LOG,
 			parent: span,
 			Level::DEBUG,
 			"starting tss",
 		);
-		self.tx_submitter
-			.submit_register_member(
-				self.task_executor.network(),
-				self.public_key.clone(),
-				self.peer_id,
-			)
-			.unwrap()
-			.unwrap();
+        while let Err(e) = self
+            .tx_submitter
+            .submit_register_member(
+                self.task_executor.network(),
+                self.public_key.clone(),
+                self.peer_id,
+            )
+            .unwrap()
+        {
+            log::error!("Error while submitting member {:?}, retrying again in 1 min", e);
+            sleep(Duration::from_secs(10)).await;
+        }
+        log::info!("Registered Member successfully");
 
 		let block = self.client.info().best_hash;
 		let min_block_time = self.runtime.runtime_api().get_block_time_in_msec(block).unwrap();

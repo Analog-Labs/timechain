f826effa1129aebe779ac2ac074e8615f0fbdf0a
Haider-Ali-DS <haiderali.tech_786@outlook.com>
2023-10-02 15:27:01 +0500

    fix bui

--- node/src/service.rs
+++ node/src/service.rs
@@ -195,6 +195,7 @@ pub fn new_full(
 			block_announce_validator_builder: None,
 			warp_sync_params: Some(sc_service::WarpSyncParams::WithProvider(warp_sync)),
 			net_config,
+			block_relay: None,
 		})?;
 
 	if config.offchain_worker.enabled {

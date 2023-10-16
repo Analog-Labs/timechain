8f6846099d249e670c40523b37fc1982ae880b81
Haider-Ali-DS <haiderali.tech_786@outlook.com>
2023-10-16 19:36:30 +0500

    metadata conflict

--- chronicle/src/shards/tests.rs
+++ chronicle/src/shards/tests.rs
@@ -324,6 +324,7 @@ async fn tss_smoke() -> Result<()> {
 			OffchainTransactionPoolFactory::new(RejectAllTxPool::default()),
 			net.peer(i).client().as_client(),
 			api.clone(),
+			None,
 		);
 
 		let n = net.peer(i).network_service().clone();

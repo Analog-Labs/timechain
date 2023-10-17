7fe5abbce12945e573ffb25e6150cc68b29f9ea2
Haider-Ali-DS <haiderali.tech_786@outlook.com>
2023-10-17 16:58:29 +0500

    fixing error

--- tc-subxt/src/lib.rs
+++ tc-subxt/src/lib.rs
@@ -7,34 +7,31 @@ use subxt::{
 	PolkadotConfig,
 };
 use subxt_signer::{bip39::Mnemonic, sr25519::Keypair, SecretUri};
-use time_primitives::{
-	Commitment, Network, PeerId, ProofOfKnowledge, PublicKey, ShardId, TaskCycle, TaskError,
-	TaskId, TaskResult,
-};
-use timechain_runtime::runtime_types::sp_runtime::MultiSigner as MetadataMultiSigner;
-use timechain_runtime::runtime_types::time_primitives::shard;
-use timechain_runtime::runtime_types::time_primitives::task;
 #[subxt::subxt(
 	runtime_metadata_path = "../config/subxt/metadata.scale",
 	derive_for_all_types = "PartialEq, Clone"
 )]
 pub mod timechain_runtime {}
+mod members;
+mod shards;
+mod tasks;
 pub type KeyPair = sp_core::sr25519::Pair;
+
 #[derive(Clone)]
 pub struct SubxtClient {
-	client: Arc<OnlineClient<PolkadotConfig>>,
+	pub client: Arc<OnlineClient<PolkadotConfig>>,
 	signer: Arc<Keypair>,
+	nonce: u64,
 }
 
 impl SubxtClient {
-	pub async fn make_transaction<Call>(&self, call: &Call) -> Vec<u8>
+	pub fn make_transaction<Call>(&self, call: &Call) -> Vec<u8>
 	where
 		Call: TxPayload,
 	{
 		self.client
 			.tx()
-			.create_signed(call, self.signer.as_ref(), Default::default())
-			.await
+			.create_signed_with_nonce(call, self.signer.as_ref(), self.nonce, Default::default())
 			.unwrap()
 			.into_encoded()
 	}
@@ -48,61 +45,8 @@ impl SubxtClient {
 		Self {
 			client: Arc::new(api),
 			signer: Arc::new(keypair),
+			nonce: 0,
 		}
-	}
-
-	pub async fn register_member(&self, network: Network, public_key: PublicKey, peer_id: PeerId) {
-		let network: shard::Network = unsafe { std::mem::transmute(network) };
-		let public_key: MetadataMultiSigner = unsafe { std::mem::transmute(public_key) };
-		let tx = timechain_runtime::tx().members().register_member(network, public_key, peer_id);
-		let encoded_tx = self.make_transaction(&tx).await;
-		self.submit_transaction(&encoded_tx).await;
-	}
-
-	pub async fn submit_commitment(
-		&self,
-		shard_id: ShardId,
-		member: PublicKey,
-		commitment: Vec<[u8; 33]>,
-		proof_of_knowledge: [u8; 65],
-	) {
-		let tx = timechain_runtime::tx()
-			.shards()
-			.commit(shard_id, commitment, proof_of_knowledge);
-		let encoded_tx = self.make_transaction(&tx).await;
-		self.submit_transaction(&encoded_tx).await;
-	}
-
-	pub async fn submit_heartbeat(&self, public_key: PublicKey) {
-		let tx = timechain_runtime::tx().members().send_heartbeat();
-		let encoded_tx = self.make_transaction(&tx).await;
-		self.submit_transaction(&encoded_tx).await;
-	}
-
-	pub async fn submit_ready(&self, shard_id: ShardId, member: PublicKey) {
-		let tx = timechain_runtime::tx().shards().ready(shard_id);
-		let encoded_tx = self.make_transaction(&tx).await;
-		self.submit_transaction(&encoded_tx).await;
-	}
-
-	pub async fn submit_tash_error(&self, task_id: TaskId, cycle: TaskCycle, error: TaskError) {
-		let error: task::TaskError = unsafe { std::mem::transmute(error) };
-		let tx = timechain_runtime::tx().tasks().submit_error(task_id, cycle, error);
-		let encoded_tx = self.make_transaction(&tx).await;
-		self.submit_transaction(&encoded_tx).await;
-	}
-
-	pub async fn submit_tash_hash(&self, task_id: TaskId, cycle: TaskCycle, hash: Vec<u8>) {
-		let tx = timechain_runtime::tx().tasks().submit_hash(task_id, cycle, hash);
-		let encoded_tx = self.make_transaction(&tx).await;
-		self.submit_transaction(&encoded_tx).await;
-	}
-
-	pub async fn submit_task_result(&self, task_id: TaskId, cycle: TaskCycle, status: TaskResult) {
-		let status: task::TaskResult = unsafe { std::mem::transmute(status) };
-		let tx = timechain_runtime::tx().tasks().submit_result(task_id, cycle, status);
-		let encoded_tx = self.make_transaction(&tx).await;
-		self.submit_transaction(&encoded_tx).await;
 	}
 
 	pub async fn submit_transaction(&self, transaction: &[u8]) {
---
+++ tc-subxt/src/shards.rs
@@ -0,0 +1,22 @@
+use time_primitives::{PublicKey, ShardId};
+
+use crate::{timechain_runtime, SubxtClient};
+
+impl SubxtClient {
+	pub fn submit_commitment(
+		&self,
+		shard_id: ShardId,
+		member: PublicKey,
+		commitment: Vec<[u8; 33]>,
+		proof_of_knowledge: [u8; 65],
+	) -> Vec<u8> {
+		let tx = timechain_runtime::tx()
+			.shards()
+			.commit(shard_id, commitment, proof_of_knowledge);
+		self.make_transaction(&tx)
+	}
+	pub fn submit_ready(&self, shard_id: ShardId, member: PublicKey) -> Vec<u8> {
+		let tx = timechain_runtime::tx().shards().ready(shard_id);
+		self.make_transaction(&tx)
+	}
+}

a065b428e13df07c11eb034b2f68c404cac63797
Haider-Ali-DS <haiderali.tech_786@outlook.com>
2023-11-07 05:15:31 +0500

    adding payload seperate logic

--- tc-subxt/src/shards.rs
+++ tc-subxt/src/shards.rs
@@ -63,11 +63,11 @@ impl ShardsPayload for SubxtClient {
 		let tx = timechain_runtime::tx()
 			.shards()
 			.commit(shard_id, commitment, proof_of_knowledge);
-		self.make_transaction(&tx)
+		self.create_signed_payload(&tx)
 	}
 
 	fn submit_online(&self, shard_id: ShardId) -> Vec<u8> {
 		let tx = timechain_runtime::tx().shards().ready(shard_id);
-		self.make_transaction(&tx)
+		self.create_signed_payload(&tx)
 	}
 }

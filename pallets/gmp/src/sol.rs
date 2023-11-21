7fb4b27bb24bee10debb0a05550fc81a718649eb
4meta5 <asinghchrony@protonmail.com>
2023-11-21 10:25:51 -0500

    update gateway interface in gmp to align with gateway dot sol

--- pallets/gmp/src/sol.rs
+++ pallets/gmp/src/sol.rs
@@ -1,11 +1,30 @@
 use sp_std::vec::Vec;
 alloy_sol_macro::sol! {
+
 	#[derive(Default, Debug, PartialEq, Eq)]
 	struct Signature {
 		uint8 parity;
 		uint256 px;
 		uint256 e;
 		uint256 s;
+	}
+
+	#[derive(Debug, PartialEq, Eq)]
+	struct TssKey {
+		uint8 parity;    // public key y-coord parity (27 or 28)
+		bytes32 coordX;  // public key x-coord
+	}
+
+	#[derive(Debug, PartialEq, Eq)]
+	struct RegisterTssKeys {
+		uint256 nonce;
+		TssKey[] keys;
+	}
+
+	#[derive(Debug, PartialEq, Eq)]
+	struct RevokeTssKeys {
+		uint256 nonce;
+		TssKey[] keys;
 	}
 
 	#[derive(Debug, PartialEq, Eq)]
@@ -18,15 +37,18 @@ alloy_sol_macro::sol! {
 	}
 
 	#[derive(Debug, PartialEq, Eq)]
-	struct TssKey {
-		uint8 parity;    // public key y-coord parity (27 or 28)
-		bytes32 coordX;  // public key x-coord
+	struct ShardInfo {
+		uint128 flags;
+		uint128 nonce;
 	}
 
 	#[derive(Debug, PartialEq, Eq)]
 	interface IGateway {
+		function sudoRegisterTSSKeys(TssKey[] memory tssKeys) external;
 		function registerTSSKeys(Signature memory signature, TssKey[] memory tssKeys) external;
+		function sudoRevokeTSSKeys(TssKey[] memory tssKeys);
 		function revokeTSSKeys(Signature memory signature, TssKey[] memory tssKeys) external;
-		function execute(Signature memory signature, GMPMessage memory message) external;
+		function sudoExecute(GMPMessage memory message) external returns (bool success);
+		function execute(Signature memory signature, GMPMessage memory message) external returns (bool success);
 	}
 }

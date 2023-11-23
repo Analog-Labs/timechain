eae080577633afad41d1050c7d8c9b4d9d24b8c2
ana-junius <junius@analog.one>
2023-11-23 18:05:04 +0700

    add mock file

--- pallets/timegraph/src/tests.rs
+++ pallets/timegraph/src/tests.rs
@@ -1,1 +1,18 @@
 
+use crate::mock::*;
+use crate::{
+	Error, Event};
+    
+use frame_support::{assert_noop, assert_ok};
+use frame_system::RawOrigin;
+
+
+#[test]
+fn test_create_task() {
+	new_test_ext().execute_with(|| {
+		// assert_ok!(Tasks::create_task(
+		
+		assert_eq!(true, false);
+		// System::assert_last_event(Event::<Test>::TaskResult(0, 0, task_result).into());
+	});
+}

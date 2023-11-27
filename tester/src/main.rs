21b963f681be9135b3f2dd115558386f3b3e5fe0
Haider-Ali-DS <haiderali.tech_786@outlook.com>
2023-11-27 23:32:48 +0500

    fixing tester bugs

--- tester/src/main.rs
+++ tester/src/main.rs
@@ -79,23 +79,21 @@ async fn main() {
 		break api;
 	};
 
+	let ethereum_config = WalletConfig {
+		blockchain: Blockchain::Ethereum,
+		network: "dev".to_string(),
+		url: "ws://ethereum:8545".to_string(),
+	};
+
+	let astar_config = WalletConfig {
+		blockchain: Blockchain::Astar,
+		network: "dev".to_string(),
+		url: "ws://astar:9944".to_string(),
+	};
+
 	let (network, config) = match args.network.as_str() {
-		"ethereum" => (
-			Network::Ethereum,
-			WalletConfig {
-				blockchain: Blockchain::Ethereum,
-				network: "dev".to_string(),
-				url: "ws://ethereum:8545".to_string(),
-			},
-		),
-		"astar" => (
-			Network::Astar,
-			WalletConfig {
-				blockchain: Blockchain::Astar,
-				network: "dev".to_string(),
-				url: "ws://astar:9944".to_string(),
-			},
-		),
+		"ethereum" => (Network::Ethereum, ethereum_config.clone()),
+		"astar" => (Network::Astar, astar_config.clone()),
 		network => panic!("unsupported network {}", network),
 	};
 
@@ -109,7 +107,7 @@ async fn main() {
 		TestCommand::BatchTask { tasks, max_cycle } => {
 			batch_test(&api, tasks, max_cycle, &config).await;
 		},
-		TestCommand::Gmp => process_gmp_task(&api, &config).await,
+		TestCommand::Gmp => process_gmp_task(&api, &ethereum_config, &astar_config).await,
 		TestCommand::KeyRecovery { nodes } => {
 			key_recovery_after_drop(&api, &config, nodes).await;
 		},
@@ -135,29 +133,68 @@ async fn main() {
 	}
 }
 
-async fn process_gmp_task(api: &SubxtClient, config: &WalletConfig) {
-	let (contract_address, start_block) = setup_env(config, true).await;
+// For GMP Task you must start both ethereum and astar nodes
+async fn process_gmp_task(
+	api: &SubxtClient,
+	eth_config: &WalletConfig,
+	astar_config: &WalletConfig,
+) {
+	let (eth_contract_address, eth_start_block) = setup_env(eth_config, true).await;
+	println!("Setup for eth done");
+	let (astar_contract_address, astar_start_block) = setup_env(astar_config, true).await;
+	println!("Setup for astar done");
 
-	while !Shards::is_shard_online(api, Network::Ethereum).await {
+	let eth_shard_id = Shards::get_shard_id(api, Network::Ethereum).await;
+	let astar_shard_id = Shards::get_shard_id(api, Network::Astar).await;
+	while !Shards::is_shard_online(api, eth_shard_id).await {
 		println!("Waiting for shard to go online");
 		tokio::time::sleep(tokio::time::Duration::from_secs(10)).await;
 	}
 
-	// get shard commitment
-	let public_key = api.shard_public_key(0).await.unwrap();
-	println!("shard key {:?}", public_key);
-	// register commitment with contract
+	while !Shards::is_shard_online(api, astar_shard_id).await {
+		println!("Waiting for shard to go online");
+		tokio::time::sleep(tokio::time::Duration::from_secs(10)).await;
+	}
+
+	register_eth_gmp(api, eth_shard_id, eth_contract_address.clone(), eth_start_block).await;
+	register_astar_gmp(api, astar_shard_id, astar_contract_address, astar_start_block).await;
+
+	let function = create_sign_task(eth_contract_address.into(), "vote_yes".into());
+	let task_id = insert_task(
+		api,
+		1, //cycle
+		0, //period
+		astar_start_block,
+		Network::Astar,
+		function,
+	)
+	.await
+	.unwrap();
+	while !watch_task(api, task_id).await {
+		tokio::time::sleep(tokio::time::Duration::from_secs(10)).await;
+	}
+}
+
+async fn register_astar_gmp(
+	api: &SubxtClient,
+	astar_shard_id: u64,
+	astar_contract_address: String,
+	astar_start_block: u64,
+) {
+	//for now we just register astar public key to do gmp tasks, so calls can be processed on ethereum shard.
+	let astar_public_key = api.shard_public_key(astar_shard_id).await.unwrap();
+	println!("astar shard key {:?}", astar_public_key);
+
 	let revoke: Vec<u8> = vec![];
 	let revoke_coord: Vec<Vec<u8>> = vec![];
 
-	let parity = vec![public_key[0]];
+	let parity = vec![astar_public_key[0]];
 	let mut x_coord = [0u8; 32];
-	x_coord.copy_from_slice(&public_key[1..]);
+	x_coord.copy_from_slice(&astar_public_key[1..]);
 	let x_coord_uint = BigUint::from_bytes_be(&x_coord);
 	let x_coord = vec![x_coord_uint];
-	println!("{:?}-{:?}", parity, x_coord);
 	let function = create_gmp_register_call(
-		contract_address.clone(),
+		astar_contract_address.clone(),
 		vec![
 			format!("{:?}", revoke),
 			format!("{:?}", revoke_coord),
@@ -165,27 +202,51 @@ async fn process_gmp_task(api: &SubxtClient, config: &WalletConfig) {
 			format!("{:?}", x_coord),
 		],
 	);
-	let task_id = insert_task(api, 1, start_block, 0, Network::Ethereum, function).await.unwrap();
+	let task_id = insert_task(api, 1, astar_start_block, 0, Network::Astar, function)
+		.await
+		.unwrap();
 	println!("task_id {:?}", task_id);
 	while !watch_task(api, task_id).await {
+		println!("waiting for astar gmp register task to complete");
 		tokio::time::sleep(tokio::time::Duration::from_secs(10)).await;
 	}
-	// then execute gmp task
-	//TODO fix which task to call
-	// let task_id = insert_sign_task(
-	// 	api,
-	// 	1, //cycle
-	// 	0, //period
-	// 	start_block,
-	// 	Network::Ethereum,
-	// 	contract_address.into(),
-	// 	"vote_yes()".into(), //payload
-	// )
-	// .await
-	// .unwrap();
-	// while !watch_task(api, task_id).await {
-	// 	tokio::time::sleep(tokio::time::Duration::from_secs(10)).await;
-	// }
+}
+
+async fn register_eth_gmp(
+	api: &SubxtClient,
+	eth_shard_id: u64,
+	eth_contract_address: String,
+	eth_start_block: u64,
+) {
+	//for now we just register eth public key to do gmp tasks, so calls can be processed on ethereum shard.
+	let eth_public_key = api.shard_public_key(eth_shard_id).await.unwrap();
+	println!("eth shard key {:?}", eth_public_key);
+
+	let revoke: Vec<u8> = vec![];
+	let revoke_coord: Vec<Vec<u8>> = vec![];
+
+	let parity = vec![eth_public_key[0]];
+	let mut x_coord = [0u8; 32];
+	x_coord.copy_from_slice(&eth_public_key[1..]);
+	let x_coord_uint = BigUint::from_bytes_be(&x_coord);
+	let x_coord = vec![x_coord_uint];
+	let function = create_gmp_register_call(
+		eth_contract_address.clone(),
+		vec![
+			format!("{:?}", revoke),
+			format!("{:?}", revoke_coord),
+			format!("{:?}", parity),
+			format!("{:?}", x_coord),
+		],
+	);
+	let task_id = insert_task(api, 1, eth_start_block, 0, Network::Ethereum, function)
+		.await
+		.unwrap();
+	println!("task_id {:?}", task_id);
+	while !watch_task(api, task_id).await {
+		println!("waiting for eth gmp register task to complete");
+		tokio::time::sleep(tokio::time::Duration::from_secs(10)).await;
+	}
 }
 
 async fn basic_test_timechain(api: &SubxtClient, network: Network, config: &WalletConfig) {
@@ -278,7 +339,8 @@ async fn task_update_after_shard_offline(api: &SubxtClient, config: &WalletConfi
 	println!("dropped 2 nodes");
 
 	// wait for some time
-	while Shards::is_shard_online(api, Network::Ethereum).await {
+	let eth_shard_id = Shards::get_shard_id(api, Network::Ethereum).await;
+	while Shards::is_shard_online(api, eth_shard_id).await {
 		println!("Waiting for shard offline");
 		tokio::time::sleep(tokio::time::Duration::from_secs(50)).await;
 	}

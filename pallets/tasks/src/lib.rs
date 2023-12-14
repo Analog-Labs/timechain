4ffd41d22c68932c6bcc3d2e2d5fc31d4792ee63
Haider-Ali-DS <haiderali.tech_786@outlook.com>
2023-12-14 23:15:25 +0500

    fix task assignment

--- pallets/tasks/src/lib.rs
+++ pallets/tasks/src/lib.rs
@@ -14,8 +14,8 @@ pub mod pallet {
 	use frame_system::pallet_prelude::*;
 	use schnorr_evm::VerifyingKey;
 	use sp_runtime::{traits::IdentifyAccount, Saturating};
-	use sp_std::vec;
 	use sp_std::vec::Vec;
+	use sp_std::{if_std, vec};
 	use time_primitives::{
 		append_hash_with_task_data, AccountId, Function, Network, ShardId, ShardsInterface,
 		TaskCycle, TaskDescriptor, TaskDescriptorParams, TaskError, TaskExecution, TaskId,

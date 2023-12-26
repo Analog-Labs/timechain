8bc34428a508be653b98dea03aa078ae46896fb8
Haider-Ali-DS <haiderali.tech_786@outlook.com>
2023-12-26 17:22:26 +0500

    adding chain_id in network

--- primitives/src/network.rs
+++ primitives/src/network.rs
@@ -1,5 +1,6 @@
 use scale_info::prelude::string::String;
 
 pub type NetworkId = u64;
-pub type NetworkBlockchain = String;
-pub type NetworkName = String;
+pub type ChainId = u64;
+pub type ChainName = String;
+pub type ChainNetwork = String;

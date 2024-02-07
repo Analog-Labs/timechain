56cd7bcc1438a26cd9e714562effcbf805fd1292
Florian Franzen <Florian.Franzen@gmail.com>
2024-02-07 13:34:37 +0100

    polkadot-sdk: 1.6.0 -> 1.7.0

--- pallets/tasks/src/mock.rs
+++ pallets/tasks/src/mock.rs
@@ -91,7 +91,6 @@ impl pallet_balances::Config for Test {
 	type RuntimeHoldReason = ();
 	type RuntimeFreezeReason = ();
 	type FreezeIdentifier = ();
-	type MaxHolds = ();
 	type MaxFreezes = ();
 }
 

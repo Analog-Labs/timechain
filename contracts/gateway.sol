4f1d50319a5efd6ed25ed3599416a9710ba38517
4meta5 <asinghchrony@protonmail.com>
2024-02-21 14:04:30 -0500

    fmt

--- contracts/gateway.sol
+++ contracts/gateway.sol
@@ -18,7 +18,10 @@ interface IGmpReceiver {
      * @param payload The message payload with no specified format
      * @return 32 byte result which will be stored together with GMP message
      */
-    function onGmpReceived(bytes32 id, uint128 network, bytes32 source, bytes calldata payload) payable external returns (bytes32);
+    function onGmpReceived(bytes32 id, uint128 network, bytes32 source, bytes calldata payload)
+        external
+        payable
+        returns (bytes32);
 }
 
 /**
@@ -28,29 +31,29 @@ interface IGateway {
     /**
      * @dev Emitted when `GmpMessage` is executed.
      */
-    event GmpExecuted(
-        bytes32 indexed id,     // EIP-712 hash of the `GmpPayload`, which is it's unique identifier
-        bytes32 indexed source, // sender pubkey/address (the format depends on src chain)
-        address indexed dest,   // recipient address
-        uint256 status,         // GMP message execution status
-        bytes32 result          // GMP result
-    );
+    event GmpExecuted( // EIP-712 hash of the `GmpPayload`, which is it's unique identifier
+        // sender pubkey/address (the format depends on src chain)
+        // recipient address
+        // GMP message execution status
+        // GMP result
+    bytes32 indexed id, bytes32 indexed source, address indexed dest, uint256 status, bytes32 result);
 
     /**
      * @dev Emitted when `UpdateShardsMessage` is executed.
      */
-    event KeySetChanged(
-        bytes32 indexed id,    // EIP-712 hash of the UpdateShardsMessage, zero for sudo
-        TssKey[] revoked,      // shards with keys revoked
-        TssKey[] registered    // new shards registered
-    );
+    event KeySetChanged( // EIP-712 hash of the UpdateShardsMessage, zero for sudo
+        // shards with keys revoked
+        // new shards registered
+    bytes32 indexed id, TssKey[] revoked, TssKey[] registered);
 
     function deposit(bytes32 source, uint128 network) external payable;
 
     /**
      * Execute GMP message
      */
-    function execute(Signature memory signature, GmpMessage memory message) external returns (uint8 status, bytes32 result);
+    function execute(Signature memory signature, GmpMessage memory message)
+        external
+        returns (uint8 status, bytes32 result);
 
     /**
      * Update TSS key set
@@ -62,7 +65,7 @@ interface IGateway {
  * @dev Tss public key
  */
 struct TssKey {
-    uint8 yParity;  // public key y-coord parity, the contract converts it to 27/28
+    uint8 yParity; // public key y-coord parity, the contract converts it to 27/28
     uint256 xCoord; // affine x-coordinate
 }
 
@@ -70,21 +73,21 @@ struct TssKey {
  * @dev Message payload used to revoke or/and register new shards
  */
 struct UpdateKeysMessage {
-    TssKey[] revoke;    // Keys to revoke
-    TssKey[] register;  // Keys to add
+    TssKey[] revoke; // Keys to revoke
+    TssKey[] register; // Keys to add
 }
 
 /**
  * @dev GMP payload, this is what the timechain creates as task payload
  */
 struct GmpMessage {
-    bytes32 source;      // Pubkey/Address of who send the GMP message
-    uint128 srcNetwork;  // Source chain identifier (for ethereum networks it is the EIP-155 chain id)
-    address dest;        // Destination/Recipient contract address
+    bytes32 source; // Pubkey/Address of who send the GMP message
+    uint128 srcNetwork; // Source chain identifier (for ethereum networks it is the EIP-155 chain id)
+    address dest; // Destination/Recipient contract address
     uint128 destNetwork; // Destination chain identifier (it's the EIP-155 chain_id for ethereum networks)
-    uint256 gasLimit;    // gas limit of the GMP call
-    uint256 salt;        // Message salt, useful for sending two messages with same content
-    bytes data;          // message data with no specified format
+    uint256 gasLimit; // gas limit of the GMP call
+    uint256 salt; // Message salt, useful for sending two messages with same content
+    bytes data; // message data with no specified format
 }
 
 /**
@@ -103,11 +106,12 @@ struct Signature {
  * OBS: the order of the attributes matters! ethereum storage is 256bit aligned, try to keep
  * the shard info below 256 bit, so it can be stored in one single storage slot.
  * reference: https://docs.soliditylang.org/en/latest/internals/layout_in_storage.html
- **/
+ *
+ */
 struct KeyInfo {
-    uint216 _gap;  // gap, so we can use later for store more information about a shard
-    uint8 status;  // status, 0 = unregisted, 1 = active, 3 = revoked
-    uint32 nonce;  // shard nonce
+    uint216 _gap; // gap, so we can use later for store more information about a shard
+    uint8 status; // status, 0 = unregisted, 1 = active, 3 = revoked
+    uint32 nonce; // shard nonce
 }
 
 /**
@@ -115,12 +119,13 @@ struct KeyInfo {
  * OBS: the order of the attributes matters! ethereum storage is 256bit aligned, try to keep
  * the attributes 256 bit aligned, ex: nonce, block and status can be read in one storage access.
  * reference: https://docs.soliditylang.org/en/latest/internals/layout_in_storage.html
- **/
+ *
+ */
 struct GmpInfo {
-    uint184 _gap;       // gap to keep status and blocknumber 256bit aligned
-    uint8 status;       // message status: NOT_FOUND | PENDING | SUCCESS | REVERT
+    uint184 _gap; // gap to keep status and blocknumber 256bit aligned
+    uint8 status; // message status: NOT_FOUND | PENDING | SUCCESS | REVERT
     uint64 blockNumber; // block in which the message was processed
-    bytes32 result;     // the result of the GMP message
+    bytes32 result; // the result of the GMP message
 }
 
 contract SigUtils {
@@ -141,145 +146,99 @@ contract SigUtils {
 
     // Computes the EIP-712 domain separador
     function computeDomainSeparator() internal view virtual returns (bytes32) {
-        return
-            keccak256(
-                abi.encode(
-                    keccak256("EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)"),
-                    keccak256("Analog Gateway Contract"),
-                    keccak256("0.1.0"),
-                    block.chainid,
-                    address(this)
-                )
-            );
+        return keccak256(
+            abi.encode(
+                keccak256("EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)"),
+                keccak256("Analog Gateway Contract"),
+                keccak256("0.1.0"),
+                block.chainid,
+                address(this)
+            )
+        );
     }
 
     // computes the hash of an array of tss keys
-    function _getTssKeyHash(TssKey memory tssKey)
-        private
-        pure
-        returns (bytes32)
-    {
-        return
-            keccak256(
-                abi.encode(
-                    keccak256("TssKey(uint8 yParity,uint256 xCoord)"),
-                    tssKey.yParity,
-                    tssKey.xCoord
-                )
-            );
+    function _getTssKeyHash(TssKey memory tssKey) private pure returns (bytes32) {
+        return keccak256(abi.encode(keccak256("TssKey(uint8 yParity,uint256 xCoord)"), tssKey.yParity, tssKey.xCoord));
     }
 
     // computes the hash of an array of tss keys
-    function _getTssKeyArrayHash(TssKey[] memory tssKeys)
-        private
-        pure
-        returns (bytes32)
-    {
+    function _getTssKeyArrayHash(TssKey[] memory tssKeys) private pure returns (bytes32) {
         bytes memory keysHashed = new bytes(tssKeys.length * 32);
         uint256 ptr;
         assembly {
             ptr := keysHashed
         }
-        for (uint256 i=0; i<tssKeys.length; i++) {
+        for (uint256 i = 0; i < tssKeys.length; i++) {
             bytes32 hash = _getTssKeyHash(tssKeys[i]);
             assembly {
                 ptr := add(ptr, 32)
                 mstore(ptr, hash)
             }
         }
-        
+
         return keccak256(keysHashed);
     }
 
     // computes the hash of the fully encoded EIP-712 message for the domain, which can be used to recover the signer
-    function _getUpdateKeysHash(UpdateKeysMessage memory message)
-        private
-        pure
-        returns (bytes32)
-    {
-        return
-            keccak256(
-                abi.encode(
-                    keccak256("UpdateKeysMessage(TssKey[] revoke,TssKey[] register)TssKey(uint8 yParity,uint256 xCoord)"),
-                    _getTssKeyArrayHash(message.revoke),
-                    _getTssKeyArrayHash(message.register)
-                )
-            );
-    }
-
-    function getUpdateKeysTypedHash(UpdateKeysMessage memory message)
-        internal
-        view
-        returns (bytes32)
-    {
+    function _getUpdateKeysHash(UpdateKeysMessage memory message) private pure returns (bytes32) {
         return keccak256(
-            abi.encodePacked(
-                "\x19\x01",
-                DOMAIN_SEPARATOR(),
-                _getUpdateKeysHash(message)
+            abi.encode(
+                keccak256("UpdateKeysMessage(TssKey[] revoke,TssKey[] register)TssKey(uint8 yParity,uint256 xCoord)"),
+                _getTssKeyArrayHash(message.revoke),
+                _getTssKeyArrayHash(message.register)
             )
         );
+    }
+
+    function getUpdateKeysTypedHash(UpdateKeysMessage memory message) internal view returns (bytes32) {
+        return keccak256(abi.encodePacked("\x19\x01", DOMAIN_SEPARATOR(), _getUpdateKeysHash(message)));
     }
 
     // computes the hash of an array of tss keys
-    function _getGmpHash(GmpMessage memory gmp)
-        private
-        pure
-        returns (bytes32)
-    {
-        return
-            keccak256(
-                abi.encode(
-                    keccak256(
-                        "GmpMessage(bytes32 source,uint128 srcNetwork,address dest,uint128 destNetwork,uint256 gasLimit,uint256 salt,bytes data,address reimburse)"
-                    ),
-                    gmp.source,
-                    gmp.srcNetwork,
-                    gmp.dest,
-                    gmp.destNetwork,
-                    gmp.gasLimit,
-                    gmp.salt,
-                    keccak256(gmp.data)
-                )
-            );
+    function _getGmpHash(GmpMessage memory gmp) private pure returns (bytes32) {
+        return keccak256(
+            abi.encode(
+                keccak256(
+                    "GmpMessage(bytes32 source,uint128 srcNetwork,address dest,uint128 destNetwork,uint256 gasLimit,uint256 salt,bytes data,address reimburse)"
+                ),
+                gmp.source,
+                gmp.srcNetwork,
+                gmp.dest,
+                gmp.destNetwork,
+                gmp.gasLimit,
+                gmp.salt,
+                keccak256(gmp.data)
+            )
+        );
     }
 
     // computes the hash of the fully encoded EIP-712 message for the domain, which can be used to recover the signer
-    function getGmpTypedHash(GmpMessage memory message)
-        public
-        view
-        returns (bytes32)
-    {
-        return keccak256(
-            abi.encodePacked(
-                "\x19\x01",
-                DOMAIN_SEPARATOR(),
-                _getGmpHash(message)
-            )
-        );
+    function getGmpTypedHash(GmpMessage memory message) public view returns (bytes32) {
+        return keccak256(abi.encodePacked("\x19\x01", DOMAIN_SEPARATOR(), _getGmpHash(message)));
     }
 }
 
 contract Gateway is IGateway, SigUtils {
-    uint8 internal constant GMP_STATUS_NOT_FOUND  = 0;   // GMP message not processed
-    uint8 internal constant GMP_STATUS_SUCCESS    = 1;   // GMP message executed successfully
-    uint8 internal constant GMP_STATUS_REVERTED   = 2;   // GMP message executed, but reverted
-    uint8 internal constant GMP_STATUS_PENDING    = 128; // GMP message is pending (used in case of reetrancy)
+    uint8 internal constant GMP_STATUS_NOT_FOUND = 0; // GMP message not processed
+    uint8 internal constant GMP_STATUS_SUCCESS = 1; // GMP message executed successfully
+    uint8 internal constant GMP_STATUS_REVERTED = 2; // GMP message executed, but reverted
+    uint8 internal constant GMP_STATUS_PENDING = 128; // GMP message is pending (used in case of reetrancy)
 
-    uint8 internal constant SHARD_ACTIVE   = (1 << 0);  // Shard active bitflag
-    uint8 internal constant SHARD_Y_PARITY = (1 << 1);  // Pubkey y parity bitflag
+    uint8 internal constant SHARD_ACTIVE = (1 << 0); // Shard active bitflag
+    uint8 internal constant SHARD_Y_PARITY = (1 << 1); // Pubkey y parity bitflag
 
     // Owner of this contract, who can execute sudo operations
     address _owner;
 
     // Shard data, maps the pubkey coordX (which is already collision resistant) to shard info.
-    mapping (bytes32 => KeyInfo) _shards;
+    mapping(bytes32 => KeyInfo) _shards;
 
     // GMP message status
-    mapping (bytes32 => GmpInfo) _messages;
+    mapping(bytes32 => GmpInfo) _messages;
 
     // Source address => Source network => Deposit Amount
-    mapping (bytes32 => mapping(uint128 => uint256)) _deposits;
+    mapping(bytes32 => mapping(uint128 => uint256)) _deposits;
 
     Schnorr _verifier;
 
@@ -299,11 +258,11 @@ contract Gateway is IGateway, SigUtils {
     }
 
     function gmpInfo(bytes32 id) external view returns (GmpInfo memory) {
-      return _messages[id];
+        return _messages[id];
     }
 
     function keyInfo(bytes32 id) external view returns (KeyInfo memory) {
-      return _shards[id];
+        return _shards[id];
     }
 
     // Check if shard exists, verify TSS signature and increment shard nonce
@@ -325,13 +284,8 @@ contract Gateway is IGateway, SigUtils {
         }
 
         // Verify Signature
-        require(_verifier.verify(
-                yParity,
-                signature.xCoord,
-                uint256(message),
-                signature.e,
-                signature.s
-            ),
+        require(
+            _verifier.verify(yParity, signature.xCoord, uint256(message), signature.e, signature.s),
             "invalid tss signature"
         );
     }
@@ -347,7 +301,7 @@ contract Gateway is IGateway, SigUtils {
         // We don't perform any arithmetic operation, except iterate a loop
         unchecked {
             // Register or activate tss key (revoked keys keep the previous nonce)
-            for (uint256 i=0; i < keys.length; i++) {
+            for (uint256 i = 0; i < keys.length; i++) {
                 // Validate y-parity bit
                 TssKey memory newKey = keys[i];
 
@@ -371,7 +325,10 @@ contract Gateway is IGateway, SigUtils {
                 } else {
                     // If the shard exists, the provided y-parity must match the original one
                     uint8 actualYParity = (status & SHARD_Y_PARITY) > 0 ? 1 : 0;
-                    require(actualYParity == yParity, "the provided y-parity doesn't match the existing y-parity, cannot register shard");
+                    require(
+                        actualYParity == yParity,
+                        "the provided y-parity doesn't match the existing y-parity, cannot register shard"
+                    );
                 }
 
                 // store the y-parity in the `KeyInfo`
@@ -396,7 +353,7 @@ contract Gateway is IGateway, SigUtils {
         // We don't perform any arithmetic operation, except iterate a loop
         unchecked {
             // Revoke tss keys
-            for (uint256 i=0; i < keys.length; i++) {
+            for (uint256 i = 0; i < keys.length; i++) {
                 TssKey memory revokedKey = keys[i];
 
                 // Read shard from storage
@@ -457,10 +414,7 @@ contract Gateway is IGateway, SigUtils {
             newKeys := register
         }
 
-        UpdateKeysMessage memory payload = UpdateKeysMessage({
-            revoke: keysToRevoke,
-            register: newKeys
-        });
+        UpdateKeysMessage memory payload = UpdateKeysMessage({revoke: keysToRevoke, register: newKeys});
         updateKeys(signature, payload);
     }
 
@@ -471,7 +425,10 @@ contract Gateway is IGateway, SigUtils {
     }
 
     // Execute GMP message
-    function _execute(bytes32 payloadHash, GmpMessage memory message) private returns (uint8 status, bytes32 result, uint256 refund) {
+    function _execute(bytes32 payloadHash, GmpMessage memory message)
+        private
+        returns (uint8 status, bytes32 result, uint256 refund)
+    {
         uint256 gasBefore = gasleft();
         require(gasBefore > message.gasLimit, "gas limit not set in tx");
         uint256 depositBefore = _deposits[message.source][message.srcNetwork];
@@ -488,11 +445,7 @@ contract Gateway is IGateway, SigUtils {
 
         // The encoded onGmpReceived call
         bytes memory data = abi.encodeWithSelector(
-            IGmpReceiver.onGmpReceived.selector,
-            payloadHash,
-            message.srcNetwork,
-            message.source,
-            message.data
+            IGmpReceiver.onGmpReceived.selector, payloadHash, message.srcNetwork, message.source, message.data
         );
 
         // Execute GMP call
@@ -506,15 +459,16 @@ contract Gateway is IGateway, SigUtils {
             let ptr := add(data, 32)
             let size := mload(data)
             // returns 1 if the call succeed, and 0 if it reverted
-            success := call(
-                gasLimit, // call gas limit (defined in the GMP message)
-                dest,     // dest address
-                0,        // value in wei to transfer (always zero for GMP)
-                ptr,      // input memory pointer
-                size,     // input size
-                output,   // output memory pointer
-                32        // output size (fixed 32 bytes)
-            )
+            success :=
+                call(
+                    gasLimit, // call gas limit (defined in the GMP message)
+                    dest, // dest address
+                    0, // value in wei to transfer (always zero for GMP)
+                    ptr, // input memory pointer
+                    size, // input size
+                    output, // output memory pointer
+                    32 // output size (fixed 32 bytes)
+                )
         }
 
         // Get Result
@@ -554,13 +508,13 @@ contract Gateway is IGateway, SigUtils {
     // Raw Execute GMP message using shard TSS signature
     function execute(
         uint256[3] memory sig,
-        bytes32 source,      // 'source' from GmpPayload within GmpMessage
-        uint128 srcNetwork,  // 'srcNetwork' from GmpPayload within GmpMessage
-        address dest,        // 'dest' from GmpPayload within GmpMessage
+        bytes32 source, // 'source' from GmpPayload within GmpMessage
+        uint128 srcNetwork, // 'srcNetwork' from GmpPayload within GmpMessage
+        address dest, // 'dest' from GmpPayload within GmpMessage
         uint128 destNetwork, // 'destNetwork' from GmpPayload within GmpMessage
-        uint256 gasLimit,    // 'gasLimit' from GmpPayload within GmpMessage
-        uint256 salt,        // 'salt' from GmpPayload within GmpMessage
-        bytes memory data   // 'data' from GmpPayload within GmpMessage
+        uint256 gasLimit, // 'gasLimit' from GmpPayload within GmpMessage
+        uint256 salt, // 'salt' from GmpPayload within GmpMessage
+        bytes memory data // 'data' from GmpPayload within GmpMessage
     ) external returns (uint8 status, bytes32 result) {
         Signature memory signature;
         assembly {

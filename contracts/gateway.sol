025212d411e029b5874e4de4893b32ac17fb9146
Haider-Ali-DS <haiderali.tech_786@outlook.com>
2024-02-28 21:50:50 +0500

    removing debugging contract event

--- contracts/gateway.sol
+++ contracts/gateway.sol
@@ -46,10 +46,6 @@ interface IGateway {
         // new shards registered
     bytes32 indexed id, TssKey[] revoked, TssKey[] registered);
 
-    event AmountDeposit(
-      bytes32 source, uint128 network  
-    );
-
     function deposit(bytes32 source, uint128 network) external payable;
 
     /**
@@ -400,7 +396,6 @@ contract Gateway is IGateway, SigUtils {
     function deposit(bytes32 source, uint128 network) public payable {
         uint256 depositBefore = _deposits[source][network];
         _deposits[source][network] = depositBefore + msg.value;
-        emit AmountDeposit( source,network);
     }
 
     // Execute GMP message

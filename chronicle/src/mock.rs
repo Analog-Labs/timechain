da66c82a1383a3a64f3ba04b8aac6faabe275f3d
David Craven <david@craven.ch>
2024-02-28 17:47:31 +0100

    Update tests.

--- chronicle/src/mock.rs
+++ chronicle/src/mock.rs
@@ -172,7 +172,6 @@ impl Mock {
 		self,
 		task_id: TaskId,
 		signature: TssSignature,
-		_chain_id: u64,
 	) -> Result<()> {
 		let mut tasks = self.tasks.lock().unwrap();
 		let task = tasks.get_mut(&task_id).unwrap();
@@ -415,16 +414,8 @@ impl Runtime for Mock {
 		Ok(())
 	}
 
-	async fn submit_task_signature(
-		&self,
-		task_id: TaskId,
-		signature: TssSignature,
-		chain_id: u64,
-	) -> Result<()> {
-		self.clone()
-			.submit_task_signature_core(task_id, signature, chain_id)
-			.await
-			.unwrap();
+	async fn submit_task_signature(&self, task_id: TaskId, signature: TssSignature) -> Result<()> {
+		self.clone().submit_task_signature_core(task_id, signature).await.unwrap();
 		Ok(())
 	}
 
@@ -442,10 +433,6 @@ impl TaskSpawner for Mock {
 				e
 			})
 			.boxed()
-	}
-
-	fn chain_id(&self) -> u64 {
-		0
 	}
 
 	fn execute_read(
@@ -481,12 +468,11 @@ impl TaskSpawner for Mock {
 		task_id: TaskId,
 		payload: Vec<u8>,
 		block_num: u32,
-		chain_id: u64,
 	) -> Pin<Box<dyn Future<Output = Result<()>> + Send + 'static>> {
 		let spawner = self.clone();
 		Box::pin(async move {
 			let (_hash, sig) = spawner.tss_sign(block_num, shard_id, task_id, &payload).await?;
-			spawner.submit_task_signature_core(task_id, sig, chain_id).await?;
+			spawner.submit_task_signature_core(task_id, sig).await?;
 			Ok(())
 		})
 	}

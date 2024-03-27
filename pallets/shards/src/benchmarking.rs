44fba8e7568ed8ad60b0a98616e9e2414f084812
4meta5 <asinghchrony@protonmail.com>
2024-03-27 17:36:51 -0400

    try fix

--- pallets/shards/src/benchmarking.rs
+++ pallets/shards/src/benchmarking.rs
@@ -4,6 +4,7 @@ use frame_benchmarking::benchmarks;
 use frame_system::RawOrigin;
 use schnorr_evm::k256::ProjectivePoint;
 use schnorr_evm::VerifyingKey;
+use sp_std::vec;
 use sp_std::vec::Vec;
 use time_primitives::{AccountId, NetworkId, ShardsInterface};
 

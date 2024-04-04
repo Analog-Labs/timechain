7dd43007f2a2b629e4b22d00788c7038711fde12
Lohann Paterno Coutinho Ferreira <lohann@enjin.io>
2024-04-04 01:35:58 -0300

    Forge fmt

--- contracts/gateway.t.sol
+++ contracts/gateway.t.sol
@@ -463,31 +463,64 @@ contract GatewayBase is Test {
 }
 
 /**
- * Hack to inject arbitrary code into the contract
+ * @dev Workaround to fix Forge the gas report.
+ *
+ * Due to limitations in forge, the gas cost reported is misleading:
+ * - https://github.com/foundry-rs/foundry/issues/6578
+ * - https://github.com/foundry-rs/foundry/issues/6910
+ *
+ * The contract is a workaround that fixes it by inject an arbitrary code into the `GatewayBase`,
+ * it replaces the constant `0x7E7E7E7E7E7E...` defined in the `_call` function by the `INLINE_BYTECODE`.
+ * This guarantees us to precisely compute the execution gas cost.
+ *
+ * This workaround is necessary while solidity doesn't support verbatim in inline assembly code.
+ * - https://github.com/ethereum/solidity/issues/12067
+ *
+ * @author Lohann Ferreira
  */
 contract GatewayTest is GatewayBase {
-    // Bytecode to inject.
-    // This do an accurate gas
+    /**
+     * @dev Bytecode that does an acurrate gas measurement of a call, it is equivalent to:
+     * ```solidity
+     * uint256 gasBefore = gasleft();
+     * contract.call{gas: gasLimit}(data);
+     * uint256 gasAfter = gasleft();
+     * uint256 gasUsed = gasBefore - gasAfter - OVERHEAD;
+     * assembly {
+     *    mstore(mload(0x40), gasUsed)
+     * }
+     * ```
+     * Solidity is a black box, is not possible to reliably calculate the `OVERHEAD` cost, creating a lot of
+     * uncertainty in the gas measurements. `Yul` have the same issue once we don't control the EVM stack.
+     * This code workaround this by doing the gas measurement right before and after execute the CALL opcode.
+     */
     bytes32 private constant INLINE_BYTECODE = 0x6a0000000000000000000000919594939291905a96f15a606901909103604051;
 
     constructor() payable {
+        // In solidity the child's constructor are executed before the parent's constructor,
+        // so once this contract extends `GatewayBase`, it's constructor is executed first.
+
+        // Copy `GatewayBase` runtime code into memory.
         bytes memory runtimeCode = type(GatewayBase).runtimeCode;
+
+        // Replaces the first occurence of `0x7E7E..` in the runtime code by the `INLINE_BYTECODE`
         assembly ("memory-safe") {
             let size := mload(runtimeCode)
             let i := add(runtimeCode, 32)
 
-            // Efficient Algorithm to find 32 repeated bytes (ex: 0x7E7E7E..) in a byte sequence
+            // Efficient Algorithm to find 32 consecutive repeated bytes in a byte sequence
             for {
-              let chunk := 1
-              let end := add(i, size)
+                let chunk := 1
+                let end := add(i, size)
             } gt(chunk, 0) { i := add(i, chunk) } {
                 // Transform all `0x7E` bytes into `0xFF`
                 // 0x81 ^ 0x7E == 0xFF
                 // Also transform all other bytes in something different than `0xFF`
                 chunk := xor(mload(i), 0x8181818181818181818181818181818181818181818181818181818181818181)
 
-                // Find the right most byte different than `0xFF` (that's why we had to convert `0x7E` into `0xFF)
-                // (0x12345678FFFFFF + 1) & (~0x12345678FFFFFF) == 0x00000001000000
+                // Find the right most unset bit, which is equivalent to find the
+                // right most byte different than `0x7E`.
+                // ex: (0x12345678FFFFFF + 1) & (~0x12345678FFFFFF) == 0x00000001000000
                 chunk := and(add(chunk, 1), not(chunk))
 
                 // Round down to the closest multiple of 256
@@ -500,8 +533,7 @@ contract GatewayTest is GatewayBase {
                 // 1337 * (2 ** 16) == 1337 << 16
                 // Once the chunk is a multiple of 256 it always shift entire bytes, we use this to
                 // select a specific byte in a byte sequence.
-                chunk :=
-                    shr(248, mul(0x201f1e1d1c1b1a191817161514131211100f0e0d0c0b0a090807060504030201, chunk))
+                chunk := shr(248, mul(0x201f1e1d1c1b1a191817161514131211100f0e0d0c0b0a090807060504030201, chunk))
 
                 // Stop the loop if we go out of bounds
                 chunk := mul(chunk, lt(i, end))
@@ -511,14 +543,17 @@ contract GatewayTest is GatewayBase {
             if not(xor(mload(i), 0x8181818181818181818181818181818181818181818181818181818181818181)) {
                 let ptr := mload(0x40)
                 mstore(ptr, shl(224, 0x08c379a0))
-                mstore(add(ptr, 4), 32)  // message offset
+                mstore(add(ptr, 4), 32) // message offset
                 mstore(add(ptr, 36), 29) // message size
                 mstore(add(ptr, 68), "Failed to inject the bytecode")
                 revert(ptr, 100)
             }
 
+            // Replace the runtime code with the injected bytecode
             mstore(add(i, 1), 0x52)
             mstore(i, INLINE_BYTECODE)
+
+            // Return the modified runtime code
             return(add(runtimeCode, 32), mload(runtimeCode))
         }
     }

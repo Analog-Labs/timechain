eee677111d0b7c3cabb7a09398995ec52823f390
David Craven <david@craven.ch>
2024-04-25 16:16:02 +0200

    Sudo reset calls. (#865)
    Co-authored-by: Github Bot <github@analog.one>

--- runtime/src/weights/tasks.rs
+++ runtime/src/weights/tasks.rs
@@ -226,4 +226,11 @@ impl<T: frame_system::Config> pallet_tasks::WeightInfo for WeightInfo<T> {
 			.saturating_add(Weight::from_parts(0, 0))
 			.saturating_add(T::DbWeight::get().writes(1))
 	}
+	fn cancel_task() -> Weight {
+		Weight::from_parts(0, 0)
+	}
+
+	fn reset_tasks() -> Weight {
+		Weight::from_parts(0, 0)
+	}
 }

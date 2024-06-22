462eb1cf94e6ad5c8100d0cf418343e1022db733
Florian Franzen <Florian.Franzen@gmail.com>
2024-06-22 23:53:01 +0200

    runtime: export metadata during build

--- runtime/build.rs
+++ runtime/build.rs
@@ -2,8 +2,14 @@ use substrate_wasm_builder::WasmBuilder;
 
 fn main() {
 	#[cfg(not(feature = "development"))]
-	WasmBuilder::init_with_defaults().enable_metadata_hash("TANLOG", 12).build();
+	WasmBuilder::init_with_defaults()
+		.enable_metadata()
+		.enable_metadata_hash("TANLOG", 12)
+		.build();
 
 	#[cfg(feature = "development")]
-	WasmBuilder::init_with_defaults().enable_metadata_hash("DANLOG", 12).build();
+	WasmBuilder::init_with_defaults()
+		.enable_metadata()
+		.enable_metadata_hash("DANLOG", 12)
+		.build();
 }

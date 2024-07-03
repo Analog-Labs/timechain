6833c094b7a431bcfb5da7e5bb3aa312aaac9c54
TaniaSaleem <47881225+TaniaSaleem@users.noreply.github.com>
2024-07-03 17:20:58 +0500

    added docs for elections, network, and timegraph pallets

--- pallets/networks/src/lib.rs
+++ pallets/networks/src/lib.rs
@@ -1,4 +1,15 @@
 #![cfg_attr(not(feature = "std"), no_std)]
+//!
+//! The network pallet manages the registration and configuration of various blockchain networks within a decentralized system. It provides functionality to add new networks, retrieve network information, and ensures each network is uniquely identified by a `NetworkId`.
+//!
+//! ##  Features
+//!
+//! - Allows privileged users to add new blockchain networks with unique `ChainName` and `ChainNetwork` combinations. This operation requires root authorization.
+//!
+//! - Maintains storage of network configurations using a mapping between `NetworkId` and `(ChainName, ChainNetwork)` tuples.
+//!
+//! - Initializes the pallet with predefined network configurations during blockchain genesis, ensuring seamless operation from the start.
+#![doc = simple_mermaid::mermaid!("../docs/network_flow.mmd")]
 
 #[cfg(feature = "runtime-benchmarks")]
 mod benchmarking;
@@ -40,12 +51,15 @@ pub mod pallet {
 	#[pallet::event]
 	#[pallet::generate_deposit(pub(super) fn deposit_event)]
 	pub enum Event<T: Config> {
+		/// Event emitted when a new network is successfully added, providing the corresponding `NetworkId` as part of the event payload.
 		NetworkAdded(NetworkId),
 	}
 
 	#[pallet::error]
 	pub enum Error<T> {
+		/// Error indicating that the generation of a new `NetworkId` has exceeded its maximum limit.
 		NetworkIdOverflow,
+		/// Error indicating that a network with the same `ChainName` and `ChainNetwork` already exists and cannot be added again.
 		NetworkExists,
 	}
 
@@ -73,6 +87,7 @@ pub mod pallet {
 		}
 	}
 
+	/// The pallet's genesis configuration (`GenesisConfig`) allows initializing the pallet with predefined network configurations. It specifies an initial list of `(ChainName, ChainNetwork)` pairs that are added to the storage during genesis.
 	#[pallet::genesis_build]
 	impl<T: Config> BuildGenesisConfig for GenesisConfig<T> {
 		fn build(&self) {
@@ -84,6 +99,16 @@ pub mod pallet {
 	}
 
 	impl<T: Config> Pallet<T> {
+		///  Inserts a new network into storage if it doesn't already exist. Updates the `NetworkIdCounter` to ensure unique network IDs.
+		///    
+		///  # Flow
+		///    1. Iterate through existing networks to check if the given `ChainName` and `ChainNetwork` already exist.
+		///    2. If the network exists, return [`Error::<T>::NetworkExists`].
+		///    3. Retrieve the current [`NetworkIdCounter`].
+		///    4. Increment the counter and check for overflow, returning [`Error::<T>::NetworkIdOverflow`] if overflow occurs.
+		///    5. Update the [`NetworkIdCounter`] with the new value.
+		///    6. Insert the new network into the [`Networks`] storage map with the current `NetworkId`.
+		///    7. Return the new `NetworkId`.
 		fn insert_network(
 			chain_name: ChainName,
 			chain_network: ChainNetwork,
@@ -108,6 +133,14 @@ pub mod pallet {
 
 	#[pallet::call]
 	impl<T: Config> Pallet<T> {
+		///  Adds a new blockchain network with a unique `ChainName` and `ChainNetwork` combination.
+		///    
+		///  # Flow
+		///    
+		///    1. Ensure the caller is the root user.
+		///    2. Call `Self::insert_network(chain_name, chain_network).
+		///    3. Emit the [`Event::NetworkAdded`] event with the new `NetworkId`.
+		///    4. Return `Ok(())` to indicate success.
 		#[pallet::call_index(0)]
 		#[pallet::weight(T::WeightInfo::add_network(chain_name.len() as u32, chain_network.len() as u32))]
 		pub fn add_network(
@@ -123,6 +156,11 @@ pub mod pallet {
 	}
 
 	impl<T: Config> Pallet<T> {
+		///  Retrieves the network information (i.e., `ChainName` and `ChainNetwork`) associated with a given `NetworkId`.
+		///    
+		///  # Flow
+		///  1. Call [`Networks`] to fetch the network information.
+		///  2. Return the network information if it exists, otherwise return `None`.
 		pub fn get_network(network_id: NetworkId) -> Option<(ChainName, ChainNetwork)> {
 			Networks::<T>::get(network_id)
 		}

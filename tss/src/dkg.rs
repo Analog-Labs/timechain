d21ab0bec1bfa07bd2d9f0b29a58aa73d957b58b
taniasaleem123 <97734319+taniasaleem123@users.noreply.github.com>
2024-07-12 15:25:19 +0500

    Research/doc (#1002)
    Co-authored-by: TaniaSaleem <47881225+TaniaSaleem@users.noreply.github.com>

--- tss/src/dkg.rs
+++ tss/src/dkg.rs
@@ -1,3 +1,12 @@
+//! # Distributed Key Generation (DKG) Module
+//! The DKG module is responsible for managing the Distributed Key Generation
+//! process, which allows a set of participants to collectively generate a
+//! shared secret key. This process involves multiple rounds of communication
+//! and ensures that no single participant has access to the entire secret key.
+//! The module supports handling messages, committing to secret shares, and
+//! transitioning through different stages of the DKG process.
+//!
+//!
 use frost_evm::frost_secp256k1::Signature;
 use frost_evm::keys::dkg::*;
 use frost_evm::keys::{
@@ -8,11 +17,16 @@ use rand_core::OsRng;
 use serde::{Deserialize, Serialize};
 use std::collections::{BTreeSet, HashMap};
 
+/// Defines different actions that can occur during the DKG process:
 #[derive(Clone)]
 pub enum DkgAction {
+	/// Send a commitment with a proof.
 	Commit(VerifiableSecretSharingCommitment, Signature),
+	/// Send messages to other participants.
 	Send(Vec<(Identifier, DkgMessage)>),
+	/// Completion of the DKG process with a key package and commitment.
 	Complete(KeyPackage, PublicKeyPackage, VerifiableSecretSharingCommitment),
+	/// Represents a failure in the process.
 	Failure(Error),
 }
 
@@ -38,7 +52,19 @@ pub struct Dkg {
 }
 
 impl Dkg {
+	/// Creates a new instance of the DKG state machine.
+	///
+	/// ### Arguments
+	///
+	/// * `id` - The identifier of the current participant.
+	/// * `members` - A set of identifiers for all participants.
+	/// * `threshold` - The threshold number of participants needed to reconstruct the secret.
+	///
+	/// ### Returns
+	///
+	/// A new `Dkg` instance.
 	pub fn new(id: Identifier, members: BTreeSet<Identifier>, threshold: u16) -> Self {
+		// Ensures the current participant is in the members set.
 		debug_assert!(members.contains(&id));
 		Self {
 			id,
@@ -51,15 +77,32 @@ impl Dkg {
 		}
 	}
 
+	/// Handles the receipt of a commitment from another participant.
+	///
+	/// ### Arguments
+	///
+	/// * `commitment` - The commitment received from another participant.
 	pub fn on_commit(&mut self, commitment: VerifiableSecretSharingCommitment) {
 		self.commitment = Some(commitment);
 	}
 
+	/// Handles the receipt of a message from another participant.
+	///
+	/// ### Arguments
+	///
+	/// * `peer` - The identifier of the peer who sent the message.
+	/// * `msg` - The message received from the peer.
 	pub fn on_message(&mut self, peer: Identifier, msg: DkgMessage) {
 		self.round2_packages.insert(peer, msg.0);
 	}
 
+	/// Determines the next action to be taken in the DKG process.
+	///
+	/// ### Returns
+	///
+	/// An optional `DkgAction` representing the next action to be taken.
 	pub fn next_action(&mut self) -> Option<DkgAction> {
+		// Check if the secret package is already initialized. If not, perform the first part of the DKG process.
 		let Some(secret_package) = self.secret_package.as_ref() else {
 			let (secret_package, round1_package) =
 				match part1(self.id, self.members.len() as _, self.threshold, OsRng) {
@@ -74,7 +117,9 @@ impl Dkg {
 				*round1_package.proof_of_knowledge(),
 			));
 		};
+		// Ensure a commitment has been received.
 		let commitment = self.commitment.as_ref()?;
+		// Prepare to send round 2 packages to peers.
 		if !self.sent_round2_packages {
 			let mut msgs = Vec::with_capacity(self.members.len());
 			for peer in &self.members {
@@ -87,10 +132,12 @@ impl Dkg {
 			self.sent_round2_packages = true;
 			return Some(DkgAction::Send(msgs));
 		}
+		// Check if all round 2 packages have been received.
 		if self.round2_packages.len() != self.members.len() - 1 {
 			return None;
 		}
 
+		// Combine all received signing shares into one signing share.
 		let signing_share = self
 			.round2_packages
 			.values()
@@ -102,6 +149,7 @@ impl Dkg {
 			.fold(SigningShare::new(Scalar::ZERO), |acc, e| {
 				SigningShare::new(acc.to_scalar() + e.to_scalar())
 			});
+		// Create a secret share and corresponding key package.
 		let secret_share = SecretShare::new(self.id, signing_share, commitment.clone());
 		let key_package = match KeyPackage::try_from(secret_share) {
 			Ok(key_package) => key_package,
@@ -109,6 +157,7 @@ impl Dkg {
 				return Some(DkgAction::Failure(error));
 			},
 		};
+		// Create a public key package from the commitment and members.
 		let public_key_package =
 			PublicKeyPackage::from_commitment(&self.members, commitment).unwrap();
 		Some(DkgAction::Complete(key_package, public_key_package, commitment.clone()))

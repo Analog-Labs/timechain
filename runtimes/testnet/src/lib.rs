4f8c9c0a81312ba0e1ea1e9c607a369d5225bb76
Florian Franzen <Florian.Franzen@gmail.com>
2024-07-25 21:02:47 +0200

    testnet-runtime: remove genesis builder by default

--- runtimes/testnet/src/lib.rs
+++ runtimes/testnet/src/lib.rs
@@ -1586,6 +1586,7 @@ impl_runtime_apis! {
 		}
 	}
 
+	#[cfg(feature = "development")]
 	impl sp_genesis_builder::GenesisBuilder<Block> for Runtime {
 		fn build_state(config: Vec<u8>) -> sp_genesis_builder::Result {
 			build_state::<RuntimeGenesisConfig>(config)

7ea96eb947e4cdec0231362df1bb54b18150a0b4
David Craven <david@craven.ch>
2024-10-01 16:48:32 +0200

    Make metadata more reliable.

--- tc-subxt/metadata/build.rs
+++ tc-subxt/metadata/build.rs
@@ -1,6 +1,23 @@
+use std::path::{Path, PathBuf};
+
+fn derive(path: &Path, module: &str) -> String {
+	format!(
+		r#"#[subxt::subxt(
+		runtime_metadata_path = "{}",
+		derive_for_all_types = "PartialEq, Clone",
+	)]
+	pub mod {} {{}}
+	"#,
+		path.display(),
+		module
+	)
+}
+
 fn main() {
-	//println!("cargo:rerun-if-changed=../../config/subxt/testnet.default.scale");
-	//println!("cargo:rerun-if-changed=../../config/subxt/testnet.development.scale");
-	//println!("cargo:rerun-if-changed=../../config/subxt/timechain.default.scale");
-	//println!("cargo:rerun-if-changed=../../config/subxt/timechain.development.scale");
+	let out_dir: PathBuf = std::env::var("OUT_DIR").unwrap().into();
+	let wbuild = out_dir.parent().unwrap().parent().unwrap().parent().unwrap().join("wbuild");
+	let mainnet = wbuild.join("mainnet-runtime").join("mainnet_runtime.metadata.scale");
+	let testnet = wbuild.join("testnet-runtime").join("testnet_runtime.metadata.scale");
+	let metadata = format!("{}\n{}", derive(&mainnet, "timechain"), derive(&testnet, "testnet"));
+	std::fs::write(out_dir.join("metadata.rs"), metadata).unwrap();
 }

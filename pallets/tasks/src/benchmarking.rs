a98b0b86f23d95dae99a54b10b6e7c49fff1b6e8
4meta5 <asinghchrony@protonmail.com>
2024-10-02 16:41:13 -0400

    fix benchmark for schedule tasks to measure on initialize only

--- pallets/tasks/src/benchmarking.rs
+++ pallets/tasks/src/benchmarking.rs
@@ -34,8 +34,6 @@ fn create_simple_task<T: Config + pallet_shards::Config>() {
 	ShardCommitment::<T>::insert(0, vec![PUBKEY]);
 	Pallet::<T>::shard_online(0, ETHEREUM);
 	Pallet::<T>::create_task(ETHEREUM, Task::ReadGatewayEvents { blocks: 0..10 });
-	Pallet::<T>::assign_task(0, 0);
-	Pallet::<T>::on_initialize(frame_system::Pallet::<T>::block_number());
 }
 
 benchmarks! {
@@ -44,17 +42,19 @@ benchmarks! {
 	submit_task_result {
 		NetworkGatewayAddress::<T>::insert(0, [0; 32]);
 		create_simple_task::<T>();
+		Pallet::<T>::assign_task(0, 0);
+		Pallet::<T>::on_initialize(frame_system::Pallet::<T>::block_number());
 		let result = TaskResult::ReadGatewayEvents { events: vec![], signature: SIGNATURE };
 	}: _(RawOrigin::Signed([0u8; 32].into()), 0, result) verify {}
 
 	schedule_tasks {
-		// TODO: replace upper bound once upper bound added, separate PR
-		let b in 1..10;
+		// TODO: replace upper bound once added in bounding PR
+		let b in 1..1000;
 		for i in 0..b {
-			// TODO: do not assign, remove on initialize
 			create_simple_task::<T>();
 		}
 	}: {
+		Pallet::<T>::assign_task(0, 0);
 		Pallet::<T>::on_initialize(frame_system::Pallet::<T>::block_number());
 	} verify { }
 

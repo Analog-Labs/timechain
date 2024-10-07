c6608b4a0b7421c3b6ca0ba9fecc1e836ea03b27
juniuszhou <junius.zhou@gmail.com>
2024-10-07 16:59:19 +0800

    update benchmarks

--- pallets/timegraph/src/benchmarking.rs
+++ pallets/timegraph/src/benchmarking.rs
@@ -30,11 +30,46 @@ mod benchmarks {
 		let caller = whitelisted_caller();
 		let amount: BalanceOf<T> = 5_000_000u32.into();
 		let amount_be: BalanceOf<T> = amount * 100u32.into();
-		let sequence = 1;
 		T::Currency::resolve_creating(&caller, T::Currency::issue(amount_be));
 		#[extrinsic_call]
 		withdraw(RawOrigin::Signed(caller), amount);
 	}
+
+	#[benchmark]
+	fn set_timegraph_account() {
+		let caller: T::AccountId = whitelisted_caller();
+		let new_account: T::AccountId = T::AccountId::from([0u8; 32]);
+		#[extrinsic_call]
+		// Example new account
+		withdraw(RawOrigin::Signed(caller), amount);
+	}
+
+	// // Benchmark for the `set_reward_pool_account` extrinsic
+	// #[benchmark]
+	// fn set_reward_pool_account() {
+	//     let caller: T::AccountId = whitelisted_caller();
+	//     let new_pool_account: T::AccountId = T::AccountId::from([1u8; 32]); // Example new pool account
+	// }: _(RawOrigin::Root, new_pool_account)
+	// verify {
+	//     // Verify that the new reward pool account is set correctly
+	//     assert_eq!(RewardPoolAccount::<T>::get(), new_pool_account);
+	// }
+
+	// // Benchmark for the `transfer_award_to_user` extrinsic
+	// #[benchmark]
+	// fn transfer_award_to_user() {
+	//     let caller: T::AccountId = whitelisted_caller();
+	//     let user_account: T::AccountId = T::AccountId::from([2u8; 32]); // Example user account
+	//     let amount: BalanceOf<T> = 100u32.into(); // Example transfer amount
+
+	//     // Ensure the reward pool has enough balance
+	//     let reward_pool_account = RewardPoolAccount::<T>::get();
+	//     T::Currency::make_free_balance_be(&reward_pool_account, 1000u32.into());
+	// }: _(RawOrigin::Signed(caller), user_account, amount)
+	// verify {
+	//     // Verify that the user's balance is updated correctly
+	//     assert_eq!(T::Currency::free_balance(&user_account), amount);
+	// }
 
 	impl_benchmark_test_suite!(Timegraph, crate::mock::new_test_ext(), crate::mock::Test);
 }

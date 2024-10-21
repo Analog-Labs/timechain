6b3b41e2d754cf80ce9efec09c91f45d2aa3d7b7
4meta5 <asinghchrony@protonmail.com>
2024-10-21 16:59:43 -0400

    fix members do heartbeat timeout

--- pallets/members/src/benchmarking.rs
+++ pallets/members/src/benchmarking.rs
@@ -60,15 +60,15 @@ benchmarks! {
 			);
 			Pallet::<T>::register_member(RawOrigin::Signed(caller.clone()).into(), ETHEREUM, pk_from_account(raw), caller.clone().into(), <T as Config>::MinStake::get())?;
 			assert!(MemberOnline::<T>::get(&caller).is_some());
+			assert!(Heartbeat::<T>::take(&caller).is_some());
 		}
 	}: {
 		Pallet::<T>::do_heartbeat_timeouts(0u32.into());
 	} verify {
-		// it is not working, consider splitting fix into separate PR if broken
-		// for i in 0..b {
-		// 	let caller: AccountId = [i as u8; 32].into();
-		// 	assert!(MemberOnline::<T>::get(&caller).is_none());
-		// }
+		for i in 0..b {
+			let caller: AccountId = [i as u8; 32].into();
+			assert!(MemberOnline::<T>::get(&caller).is_none());
+		}
 	}
 
 	impl_benchmark_test_suite!(Pallet, crate::mock::new_test_ext(), crate::mock::Test);

67656edbf0221220bf6f1efd20f4ff913f55e2d2
4meta5 <asinghchrony@protonmail.com>
2024-10-29 13:26:24 -0400

    reset weights for both runtimes

--- runtimes/mainnet/src/weights/mod.rs
+++ runtimes/mainnet/src/weights/mod.rs
@@ -7,11 +7,3 @@ pub mod networks;
 pub mod shards;
 pub mod tasks;
 pub mod timegraph;
-
-// FRAME
-// do not uncomment until configured in runtime
-//pub mod babe;
-//pub mod grandpa;
-//pub mod balances;
-//pub mod system;
-//pub mod timestamp;
--- runtimes/testnet/src/weights/mod.rs
+++ runtimes/testnet/src/weights/mod.rs
@@ -1,17 +1,14 @@
 //! A list of the different weight modules for our runtime.
 
+// pub mod babe;
+pub mod balances;
 pub mod dmail;
+// pub mod grandpa;
 pub mod elections;
 pub mod members;
 pub mod networks;
 pub mod shards;
+pub mod system;
 pub mod tasks;
 pub mod timegraph;
-
-// FRAME
-// do not uncomment until configured in runtime
-//pub mod babe;
-//pub mod grandpa;
-pub mod balances;
-pub mod system;
 pub mod timestamp;

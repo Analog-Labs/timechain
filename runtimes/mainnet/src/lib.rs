56fa6be27c9d8f48bb162e8f8bd65041c0daf3df
4meta5 <asinghchrony@protonmail.com>
2024-11-20 13:56:07 -0500

    maximize assignments with new weights

--- runtimes/mainnet/src/lib.rs
+++ runtimes/mainnet/src/lib.rs
@@ -1323,7 +1323,7 @@ impl pallet_elections::Config for Runtime {
 	type Members = Members;
 	type Shards = Shards;
 	type Networks = Networks;
-	type MaxElectionsPerBlock = ConstU32<15>;
+	type MaxElectionsPerBlock = ConstU32<18>;
 }
 
 impl pallet_shards::Config for Runtime {

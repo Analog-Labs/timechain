d5e63b6df3e802fb9df28a9de7b8c10580e7233f
Amar Singh <asinghchrony@protonmail.com>
2024-11-26 09:14:48 -0500

    Optimize elections storage to increase MaxElectionsPerBlock (#1296)

--- runtimes/testnet/src/lib.rs
+++ runtimes/testnet/src/lib.rs
@@ -988,7 +988,7 @@ impl pallet_elections::Config for Runtime {
 	type Members = Members;
 	type Shards = Shards;
 	type Networks = Networks;
-	type MaxElectionsPerBlock = ConstU32<10>;
+	type MaxElectionsPerBlock = ConstU32<69>;
 }
 
 impl pallet_shards::Config for Runtime {

bcaa98bb8f2cd85cd2af1a6573696c7f4a06eb64
Florian Franzen <Florian.Franzen@gmail.com>
2025-01-19 11:07:02 +0100

    airdrop: use hardcoded signature in benches to support nostd

--- pallets/airdrop/src/benchmarks.rs
+++ pallets/airdrop/src/benchmarks.rs
@@ -1,6 +1,6 @@
 //! Benchmarks suite for airdrop pallet
 
-use crate::{Call, Claims, Config, Pallet};
+use crate::{Call, Claims, Config, Pallet, RawSignature};
 
 use scale_codec::{Decode, Encode};
 
@@ -32,6 +32,18 @@ fn create_claim<T: Config>(ctx: &'static str, src: u32) -> DispatchResult {
 	Ok(())
 }
 
+// Pre-signed signatures, as signing requires std environment
+const SCHNORR_PROOF: RawSignature = [
+	46, 61, 191, 97, 42, 149, 19, 91, 169, 167, 82, 114, 64, 62, 141, 4, 205, 129, 88, 79, 2, 219,
+	168, 171, 212, 148, 212, 50, 174, 145, 213, 57, 8, 139, 43, 137, 72, 172, 70, 246, 145, 5, 4,
+	22, 212, 107, 72, 72, 208, 32, 25, 82, 80, 247, 132, 1, 181, 208, 86, 220, 248, 166, 185, 130,
+];
+const EDWARDS_PROOF: RawSignature = [
+	65, 15, 58, 88, 56, 207, 111, 92, 69, 45, 52, 127, 5, 32, 116, 253, 35, 25, 22, 169, 228, 142,
+	91, 131, 226, 131, 79, 231, 77, 251, 158, 88, 241, 34, 136, 20, 153, 197, 26, 237, 8, 244, 63,
+	14, 72, 93, 129, 186, 106, 38, 137, 245, 156, 163, 221, 195, 146, 205, 33, 77, 232, 183, 86, 5,
+];
+
 benchmarks! {
 	// Benchmark `claim_raw` including `validate_unsigned` logic.
 	claim_raw {
@@ -50,10 +62,11 @@ benchmarks! {
 		assert_eq!(Claims::<T>::get(source.clone()), Some(VALUE.into()));
 
 		let target: T::AccountId = account("target", 0, SEED);
-		let proof = SrClaimer.sign(&Pallet::<T>::to_message(&target)[..]).0;
+		// This is how the proof was generate, but requires std
+		//let proof = SrClaimer.sign(&Pallet::<T>::to_message(&target)[..]).0;
 
 		let txsource = sp_runtime::transaction_validity::TransactionSource::External;
-		let call_enc = Call::<T>::claim_raw { source: source.clone(), proof, target }.encode();
+		let call_enc = Call::<T>::claim_raw { source: source.clone(), proof: SCHNORR_PROOF, target }.encode();
 	}: {
 		let call = <Call<T> as Decode>::decode(&mut &*call_enc)
 			.expect("call is encoded above, encoding must be correct");
@@ -82,10 +95,11 @@ benchmarks! {
 		assert_eq!(Claims::<T>::get(source.clone()), Some(VALUE.into()));
 
 		let target: T::AccountId = account("target", 0, SEED);
-		let proof = EdClaimer.sign(&Pallet::<T>::to_message(&target)[..]).0;
+		// This is how the proof was generate, but requires std
+		//let proof = EdClaimer.sign(&Pallet::<T>::to_message(&target)[..]).0;
 
 		let txsource = sp_runtime::transaction_validity::TransactionSource::External;
-		let call_enc = Call::<T>::claim_raw { source: source.clone(), proof, target }.encode();
+		let call_enc = Call::<T>::claim_raw { source: source.clone(), proof: EDWARDS_PROOF, target }.encode();
 	}: {
 		let call = <Call<T> as Decode>::decode(&mut &*call_enc)
 			.expect("call is encoded above, encoding must be correct");

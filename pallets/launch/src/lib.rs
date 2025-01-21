b5f5cac679102455fc642f1ca627932143c17a5a
Florian Franzen <Florian.Franzen@gmail.com>
2025-01-21 20:06:56 +0100

    launch: add airdrop snapshot 2 (#1418)

--- pallets/launch/src/lib.rs
+++ pallets/launch/src/lib.rs
@@ -39,7 +39,7 @@ pub mod pallet {
 	use sp_std::{vec, vec::Vec};
 
 	/// Updating this number will automatically execute the next launch stages on update
-	const LAUNCH_STAGE: StorageVersion = StorageVersion::new(7);
+	const LAUNCH_STAGE: StorageVersion = StorageVersion::new(8);
 
 	/// Workaround to get raw storage version
 	pub fn on_chain_launch_stage<P: PalletInfoAccess>() -> u16 {
@@ -95,9 +95,9 @@ pub mod pallet {
 				Pallet::<T>::deposit_event(Event::<T>::MigrationStarted { stage });
 
 				weight += match stage {
-					6 => DepositMigration::<T>::new(data::v7::DEPOSITS_PRELAUNCH_2).execute(),
-					//7 => AirdropMigration::<T>::new(data::v8::AIRDROP_VALIDATORS).execute(),
-					//8 => DepositMigration::<T>::new(data::v9::DEPOSITS_TOKEN_GENESIS_EVENT).execute(),
+					7 => AirdropMigration::<T>::new(data::v8::AIRDROPS_SNAPSHOT_2).execute(),
+					//8 => AirdropMigration::<T>::new(data::v9::AIRDROP_VALIDATORS).execute(),
+					//9 => DepositMigration::<T>::new(data::v10::DEPOSITS_TOKEN_GENESIS_EVENT).execute(),
 					_ => break,
 				};
 

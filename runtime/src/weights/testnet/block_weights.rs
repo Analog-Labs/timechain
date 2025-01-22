55d21de86c10f3bc84e612cab571f06315b6a38d
Alexander Gryaznov <hi@agryaznov.com>
2025-01-22 20:58:04 +0200

    benchmark overhead result

--- runtime/src/weights/testnet/block_weights.rs
+++ runtime/src/weights/testnet/block_weights.rs
@@ -1,43 +1,46 @@
-//! THIS FILE WAS AUTO-GENERATED USING THE SUBSTRATE BENCHMARK CLI VERSION 4.0.0-dev
-//! DATE: 2023-05-22 (Y/M/D)
-//! HOSTNAME: `ip-172-31-31-43.us-east-2.compute.internal`, CPU: `Intel(R) Xeon(R) Platinum 8151 CPU @ 3.40GHz`
+
+//! THIS FILE WAS AUTO-GENERATED USING THE SUBSTRATE BENCHMARK CLI VERSION 42.0.1
+//! DATE: 2025-01-22 (Y/M/D)
+//! HOSTNAME: `station`, CPU: `AMD Ryzen Threadripper PRO 5945WX 12-Cores`
 //!
-//! SHORT-NAME: `block`, LONG-NAME: `BlockExecution`, RUNTIME: `Local Testnet`
+//! SHORT-NAME: `block`, LONG-NAME: `BlockExecution`, RUNTIME: `Analog Testnet Local`
 //! WARMUPS: `10`, REPEAT: `100`
-//! WEIGHT-PATH: ``
+//! WEIGHT-PATH: `runtime/src/weights/testnet/`
 //! WEIGHT-METRIC: `Average`, WEIGHT-MUL: `1.0`, WEIGHT-ADD: `0`
 
 // Executed Command:
-//   ../target/release/timechain-node
+//   target/release/timechain-node
 //   benchmark
 //   overhead
-
-use polkadot_sdk::*;
+//   --dev
+//   --wasm-execution=compiled
+//   --weight-path
+//   runtime/src/weights/testnet/
 
 use sp_core::parameter_types;
-use sp_weights::{constants, Weight};
+use sp_weights::{constants::WEIGHT_REF_TIME_PER_NANOS, Weight};
 
 parameter_types! {
 	/// Time to execute an empty block.
 	/// Calculated by multiplying the *Average* with `1.0` and adding `0`.
 	///
 	/// Stats nanoseconds:
-	///   Min, Max: 538_966, 569_416
-	///   Average:  545_181
-	///   Median:   542_327
-	///   Std-Dev:  6865.03
+	///   Min, Max: 264_567, 352_513
+	///   Average:  280_089
+	///   Median:   276_370
+	///   Std-Dev:  14162.34
 	///
 	/// Percentiles nanoseconds:
-	///   99th: 567_949
-	///   95th: 558_789
-	///   75th: 544_671
+	///   99th: 349_277
+	///   95th: 296_858
+	///   75th: 286_769
 	pub const BlockExecutionWeight: Weight =
-		Weight::from_parts(constants::WEIGHT_REF_TIME_PER_NANOS.saturating_mul(545_181), 0);
+		Weight::from_parts(WEIGHT_REF_TIME_PER_NANOS.saturating_mul(280_089), 0);
 }
 
 #[cfg(test)]
 mod test_weights {
-	use super::*;
+	use sp_weights::constants;
 
 	/// Checks that the weight exists and is sane.
 	// NOTE: If this test fails but you are sure that the generated values are fine,
--- runtime/src/weights/testnet/extrinsic_weights.rs
+++ runtime/src/weights/testnet/extrinsic_weights.rs
@@ -1,43 +1,46 @@
-//! THIS FILE WAS AUTO-GENERATED USING THE SUBSTRATE BENCHMARK CLI VERSION 4.0.0-dev
-//! DATE: 2023-05-22 (Y/M/D)
-//! HOSTNAME: `ip-172-31-31-43.us-east-2.compute.internal`, CPU: `Intel(R) Xeon(R) Platinum 8151 CPU @ 3.40GHz`
+
+//! THIS FILE WAS AUTO-GENERATED USING THE SUBSTRATE BENCHMARK CLI VERSION 42.0.1
+//! DATE: 2025-01-22 (Y/M/D)
+//! HOSTNAME: `station`, CPU: `AMD Ryzen Threadripper PRO 5945WX 12-Cores`
 //!
-//! SHORT-NAME: `extrinsic`, LONG-NAME: `ExtrinsicBase`, RUNTIME: `Local Testnet`
+//! SHORT-NAME: `extrinsic`, LONG-NAME: `ExtrinsicBase`, RUNTIME: `Analog Testnet Local`
 //! WARMUPS: `10`, REPEAT: `100`
-//! WEIGHT-PATH: ``
+//! WEIGHT-PATH: `runtime/src/weights/testnet/`
 //! WEIGHT-METRIC: `Average`, WEIGHT-MUL: `1.0`, WEIGHT-ADD: `0`
 
 // Executed Command:
-//   ../target/release/timechain-node
+//   target/release/timechain-node
 //   benchmark
 //   overhead
-
-use polkadot_sdk::*;
+//   --dev
+//   --wasm-execution=compiled
+//   --weight-path
+//   runtime/src/weights/testnet/
 
 use sp_core::parameter_types;
-use sp_weights::{constants, Weight};
+use sp_weights::{constants::WEIGHT_REF_TIME_PER_NANOS, Weight};
 
 parameter_types! {
 	/// Time to execute a NO-OP extrinsic, for example `System::remark`.
 	/// Calculated by multiplying the *Average* with `1.0` and adding `0`.
 	///
 	/// Stats nanoseconds:
-	///   Min, Max: 264_761, 269_957
-	///   Average:  266_152
-	///   Median:   265_731
-	///   Std-Dev:  1335.03
+	///   Min, Max: 92_619, 100_897
+	///   Average:  93_837
+	///   Median:   93_247
+	///   Std-Dev:  1525.55
 	///
 	/// Percentiles nanoseconds:
-	///   99th: 269_935
-	///   95th: 269_070
-	///   75th: 266_322
+	///   99th: 98_443
+	///   95th: 97_655
+	///   75th: 93_800
 	pub const ExtrinsicBaseWeight: Weight =
-		Weight::from_parts(constants::WEIGHT_REF_TIME_PER_NANOS.saturating_mul(266_152), 0);
+		Weight::from_parts(WEIGHT_REF_TIME_PER_NANOS.saturating_mul(93_837), 0);
 }
 
 #[cfg(test)]
 mod test_weights {
-	use super::*;
+	use sp_weights::constants;
 
 	/// Checks that the weight exists and is sane.
 	// NOTE: If this test fails but you are sure that the generated values are fine,

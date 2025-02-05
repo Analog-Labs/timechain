1cba299cd841b4c11c9063a26da94f4d6e9b3cb3
Alexander Gryaznov <hi@agryaznov.com>
2025-02-05 17:49:32 +0200

    update 2 benches

--- pallets/assets-bridge/src/benchmarking.rs
+++ pallets/assets-bridge/src/benchmarking.rs
@@ -25,13 +25,12 @@ mod benchmarks {
 	fn teleport_keep_alive() {
 		let caller = whitelisted_caller();
 		let amount_init: BalanceOf<T> = (1_000_000_000_000 * ANLOG).into();
-		let amount_teleport: BalanceOf<T> = (1 * ANLOG).into();
+		let amount_teleport: BalanceOf<T> = (10 * ANLOG).into();
 		T::Currency::resolve_creating(&caller, T::Currency::issue(amount_init));
 
 		let _ = Pallet::<T>::do_register_network(
 			ETHEREUM.into(),
-			// TODO add fee here
-			Default::default(),
+			(1 * ANLOG).into(),
 			(0, [0u8; 32]).into(),
 		);
 
@@ -43,19 +42,36 @@ mod benchmarks {
 	fn force_teleport() {
 		let caller = whitelisted_caller();
 		let amount_init: BalanceOf<T> = (1_000_000_000_000 * ANLOG).into();
-		let amount_teleport: BalanceOf<T> = (1 * ANLOG).into();
+		let amount_teleport: BalanceOf<T> = (10 * ANLOG).into();
 		T::Currency::resolve_creating(&caller, T::Currency::issue(amount_init));
 
 		let _ = Pallet::<T>::do_register_network(
 			ETHEREUM.into(),
-			// TODO add fee here
-			Default::default(),
+			(1 * ANLOG).into(),
 			(0, [0u8; 32]).into(),
 		);
 
 		#[extrinsic_call]
 		_(RawOrigin::Root, caller.into(), ETHEREUM.into(), ALICE.into(), amount_teleport);
 	}
+
+	// #[benchmark]
+	// fn register_network() {
+	// 	let caller = whitelisted_caller();
+	// 	let amount_init: BalanceOf<T> = (1_000_000_000_000 * ANLOG).into();
+	// 	let amount_teleport: BalanceOf<T> = (1 * ANLOG).into();
+	// 	T::Currency::resolve_creating(&caller, T::Currency::issue(amount_init));
+
+	// 	let _ = Pallet::<T>::do_register_network(
+	// 		ETHEREUM.into(),
+	// 		// TODO add fee here
+	// 		Default::default(),
+	// 		(0, [0u8; 32]).into(),
+	// 	);
+
+	// 	#[extrinsic_call]
+	// 	_(RawOrigin::Root, ETHEREUM.into(), ALICE.into(), amount_teleport);
+	// }
 
 	impl_benchmark_test_suite!(Pallet, crate::mock::new_test_ext(), crate::mock::Test);
 }

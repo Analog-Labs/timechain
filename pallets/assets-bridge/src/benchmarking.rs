0a6d15947e28e260cbcda869af1184b3d1690c47
Alexander Gryaznov <hi@agryaznov.com>
2025-02-05 17:51:17 +0200

    add bench register_network

--- pallets/assets-bridge/src/benchmarking.rs
+++ pallets/assets-bridge/src/benchmarking.rs
@@ -55,23 +55,11 @@ mod benchmarks {
 		_(RawOrigin::Root, caller.into(), ETHEREUM.into(), ALICE.into(), amount_teleport);
 	}
 
-	// #[benchmark]
-	// fn register_network() {
-	// 	let caller = whitelisted_caller();
-	// 	let amount_init: BalanceOf<T> = (1_000_000_000_000 * ANLOG).into();
-	// 	let amount_teleport: BalanceOf<T> = (1 * ANLOG).into();
-	// 	T::Currency::resolve_creating(&caller, T::Currency::issue(amount_init));
-
-	// 	let _ = Pallet::<T>::do_register_network(
-	// 		ETHEREUM.into(),
-	// 		// TODO add fee here
-	// 		Default::default(),
-	// 		(0, [0u8; 32]).into(),
-	// 	);
-
-	// 	#[extrinsic_call]
-	// 	_(RawOrigin::Root, ETHEREUM.into(), ALICE.into(), amount_teleport);
-	// }
+	#[benchmark]
+	fn register_network() {
+		#[extrinsic_call]
+		_(RawOrigin::Root, ETHEREUM.into(), (1 * ANLOG).into(), (0, [0u8; 32]).into());
+	}
 
 	impl_benchmark_test_suite!(Pallet, crate::mock::new_test_ext(), crate::mock::Test);
 }

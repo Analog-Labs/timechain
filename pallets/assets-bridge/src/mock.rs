7a675f0f172975a4933a8d167b56d4d0f7b61754
Alexander Gryaznov <hi@agryaznov.com>
2025-02-06 23:38:35 +0200

    fix pallet bridge testing mock

--- pallets/assets-bridge/src/mock.rs
+++ pallets/assets-bridge/src/mock.rs
@@ -345,6 +345,7 @@ impl pallet_tasks::Config for Test {
 	type Networks = MockNetworks;
 	type MaxTasksPerBlock = ConstU32<3>;
 	type MaxBatchesPerBlock = ConstU32<4>;
+	type Teleporter = ();
 }
 
 impl<LocalCall> frame_system::offchain::CreateSignedTransaction<LocalCall> for Test

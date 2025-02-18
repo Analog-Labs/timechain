6ecb5bbce3517d96b598932d564cb392fef8a8f7
Florian Franzen <Florian.Franzen@gmail.com>
2025-02-18 13:46:30 +0100

    nomination-pools: focus on zero wallets only

--- runtime/src/migrations/fix_pool_members.rs
+++ runtime/src/migrations/fix_pool_members.rs
@@ -1,21 +1,31 @@
 //! Runtime migration to remove buggy pool members
+use polkadot_sdk::*;
+
 use frame_support::{
 	traits::{fungible::Inspect, Get},
-	weights::Weight,
+	weights::Weight, ensure,
 };
 use pallet_nomination_pools::{
-	adapter::{Pool, StakeStrategy},
-	BondedPools, PoolMembers, TotalValueLocked,
+	BondedPools, PoolMembers,
 };
-use polkadot_sdk::*;
 use sp_core::crypto::Ss58Codec;
+use sp_runtime::traits::Zero;
 use sp_runtime::AccountId32;
-use sp_std::vec::Vec;
+
+const CORRUPTED_POOL_MEMBERS: &'static [&'static str] = &[
+	"an67vKKCtXjWYUaErzKp5E97dDnyt39HNZMwTAReK1PLUVdbA",
+	"an9XX2NvQXRvFBwMdNiaaXbAKHN4tA6oi7xGVJbffR84AvcDw",
+];
 
 // Type alias for balance type
 type BalanceOf<T> = <<T as pallet_nomination_pools::Config>::Currency as Inspect<
 	<T as frame_system::Config>::AccountId,
 >>::Balance;
+
+/// Helper function to ensure consistency
+fn total_balance_members<T: pallet_nomination_pools::Config>() -> BalanceOf<T> {
+	PoolMembers::<T>::iter().fold(Default::default(), |acc, (_i, d)| acc + d.total_balance())
+}
 
 pub struct RemoveBuggyPoolMembersMigration<T>(sp_std::marker::PhantomData<T>);
 
@@ -27,78 +37,49 @@ where
 	fn on_runtime_upgrade() -> Weight {
 		let mut weight: Weight = Weight::zero();
 
-		let to_remove = [
-			"an67vKKCtXjWYUaErzKp5E97dDnyt39HNZMwTAReK1PLUVdbA",
-			"an9XX2NvQXRvFBwMdNiaaXbAKHN4tA6oi7xGVJbffR84AvcDw",
-			"an9banSywn1r4nUkpVvFaBU5Jn5T32W9YHVvG33qo7AsrC1ci",
-			"an9RkL7gJoDerqU71knygQNzPC3Gb1gx5cTJha7h1xinLDWwc",
-		];
-		let accounts_to_remove = to_remove
-			.into_iter()
-			.filter_map(|a| {
-				if let Ok(acc) = AccountId32::from_ss58check(a) {
-					Some(acc.into())
+		// Wrap storage migration indside transactions that reverts on failure
+		if let Err(error) = frame_support::storage::with_storage_layer(|| {
+			let balance_before = total_balance_members::<T>();
+
+			for account in CORRUPTED_POOL_MEMBERS {
+				// Attempt to parse address
+				let account: T::AccountId = AccountId32::from_ss58check(account).map_err(|_|"Failed to parse address")?.into();
+
+				if let Some(member) = PoolMembers::<T>::take(&account) {
+
+					// Track weight of pool members update
+					weight += T::DbWeight::get().reads_writes(1, 1);
+
+					// Ensure this is actually an offending account
+					ensure!(member.points == Zero::zero(), "Member points not zero");
+					ensure!(member.total_balance() == Zero::zero(), "Member points not zero");
+
+					// Update the member_counter in the affected pool
+					BondedPools::<T>::mutate(member.pool_id, |maybe_pool| {
+						// Some consistency checks
+						let pool = maybe_pool.as_mut().ok_or("Member pool missing")?;
+						ensure!(pool.member_counter > 0, "Member count missmatch");
+
+						// Decrement member counter
+						pool.member_counter -= 1;
+
+						Ok::<(), &str>(())
+					})?;
+
+					// Track weight of member counter update
+					weight += T::DbWeight::get().reads_writes(1, 1);
+
+					log::info!("Successfully removed pool member: {:?}", account.clone());
 				} else {
-					log::info!("FixPoolMembersMigration: parsing failed for: {a:?}");
-					None
+					log::error!("Failed to locate pool member: {account:?}");
 				}
-			})
-			.collect::<Vec<T::AccountId>>();
-
-		// Track affected pools to update member_counter
-		let mut affected_pools =
-			sp_std::collections::btree_map::BTreeMap::<u32, (u32, BalanceOf<T>)>::new();
-
-		for account in &accounts_to_remove {
-			if let Some(member) = PoolMembers::<T>::take(account) {
-				log::info!("Removed pool member: {account:?}");
-				weight += T::DbWeight::get().writes(1);
-				let pool_id = member.pool_id; // Extract the pool ID correctly
-				let stake_amount = member.points; // Get the stake amount in raw balance
-
-				// Track both removed count and removed stake for each pool
-				affected_pools
-					.entry(pool_id)
-					.and_modify(|(count, stake)| {
-						*count += 1;
-						*stake += stake_amount;
-					})
-					.or_insert((1, stake_amount));
-				weight += T::DbWeight::get().writes(1);
 			}
-		}
 
-		// Update the member_counter and TVL for each affected pool
-		for (pool_id, (removed_members, removed_stake)) in affected_pools.iter() {
-			BondedPools::<T>::mutate(*pool_id, |maybe_pool| {
-				if let Some(pool) = maybe_pool {
-					// Decrement member counter correctly by the number of removed members
-					if pool.member_counter >= *removed_members {
-						pool.member_counter -= removed_members;
-					} else {
-						pool.member_counter = 0;
-					}
+			ensure!(total_balance_members::<T>() == balance_before, "Total member balance was changed.");
 
-					// Ensure TVL does not go negative
-					// if pool.points >= *removed_stake {
-					// 	pool.points -= *removed_stake;
-					// } else {
-					// 	pool.points = Zero::zero();
-					// }
-					if let Err(e) = <T as pallet_nomination_pools::Config>::StakeAdapter::unbond(
-						Pool::from(pallet_nomination_pools::Pallet::<T>::generate_bonded_account(
-							*pool_id,
-						)),
-						*removed_stake,
-					) {
-						log::info!(
-							"Failed with error {e:?} to unbond from pool {pool_id:?} amount {removed_stake:?}"
-						);
-					}
-					TotalValueLocked::<T>::mutate(|x| *x -= *removed_stake);
-				}
-			});
-			weight += T::DbWeight::get().writes(1);
+			Ok::<(), &str>(())
+		}) {
+			log::error!("Migration failed: {error}");
 		}
 
 		weight

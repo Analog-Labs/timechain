9a57163b5a537c9bc4599f9fee8b7eafebdb0671
David Craven <david@craven.ch>
2025-03-02 12:29:00 +0100

    Support 25/25.

--- chronicle/src/network/protocol.rs
+++ chronicle/src/network/protocol.rs
@@ -15,8 +15,8 @@ struct TssProtocol;
 
 impl Protocol for TssProtocol {
 	const ID: u16 = 0;
-	const REQ_BUF: usize = 2048;
-	const RES_BUF: usize = 2048;
+	const REQ_BUF: usize = 4096;
+	const RES_BUF: usize = 4096;
 	type Request = Message;
 	type Response = Message;
 }

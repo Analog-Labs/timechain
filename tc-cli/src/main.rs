d5b72773da6942e396a5fc3db4ef872b7efc2862
David Craven <david@craven.ch>
2025-03-08 11:42:18 +0100

    Assert invariants before test.

--- tc-cli/src/main.rs
+++ tc-cli/src/main.rs
@@ -438,6 +438,8 @@ async fn real_main() -> Result<()> {
 		},
 		Command::SmokeTest { src, dest } => {
 			let testers = tc.setup_test().await?;
+			tc.assert_reimburstment().await?;
+			tc.assert_message_fees().await?;
 			let _ = exec_smoke(&tc, src, dest, &testers, vec![42]).await?;
 			tc.assert_reimburstment().await?;
 			tc.assert_message_fees().await?;

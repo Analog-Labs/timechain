ce04393f11b2ff078c8531ca930f3f042410736f
David Craven <david@craven.ch>
2025-03-08 10:09:52 +0100

    Fix clippy.

--- chronicle/src/lib.rs
+++ chronicle/src/lib.rs
@@ -80,7 +80,7 @@ pub fn init_opentelemetry() {
 			.add_directive("tss=debug".parse().unwrap())
 			.add_directive("peernet=debug".parse().unwrap());
 		tracing_subscriber::registry()
-			.with(LevelFilter::INFO.into())
+			.with(LevelFilter::INFO)
 			.with(log_subscriber)
 			.with(filter)
 			.try_init()

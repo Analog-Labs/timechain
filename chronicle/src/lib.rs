959ace9778678f84f8a53b1f800fc6d2933125d7
Haider-Ali-DS <haiderali.tech_786@outlook.com>
2025-03-17 20:24:06 +0500

    revert unintended changes

--- chronicle/src/lib.rs
+++ chronicle/src/lib.rs
@@ -133,8 +133,8 @@ pub async fn run_chronicle(
 		if let Some(network) = network {
 			break network;
 		}
-		ticker.next().await;
 		tracing::warn!("network {} isn't registered", config.network_id);
+		ticker.next().await;
 	};
 	let (tss_tx, tss_rx) = mpsc::channel(10);
 	let blockchain = String::decode(&mut chain.0.to_vec().as_slice()).unwrap_or_default();

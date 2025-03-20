41216e547f43cc529444207eb07e4aab70dd55e3
Branimir Malesevic <glbranimir@gmail.com>
2025-03-20 12:35:33 +0100

    chronicle: enter parent spans when in different thread (#1713)

--- chronicle/src/shards/service.rs
+++ chronicle/src/shards/service.rs
@@ -373,6 +373,7 @@ where
 		loop {
 			futures::select! {
 				notification = block_notifications.next().fuse() => {
+					let _enter = span.enter();
 					let Some((_block_hash, block)) = notification else {
 						event!(
 							parent: span,
@@ -414,6 +415,7 @@ where
 					}
 				},
 				notification = finality_notifications.next().fuse() => {
+					let _enter = span.enter();
 					let Some((block_hash, block)) = notification else {
 						event!(
 							parent: span,
@@ -432,6 +434,7 @@ where
 					}
 				},
 				tss_request = self.tss_request.next().fuse() => {
+					let _enter = span.enter();
 					let Some(TssSigningRequest { task_id, shard_id, data, tx, block }) = tss_request else {
 						continue;
 					};

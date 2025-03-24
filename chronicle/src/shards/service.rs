40c8ccdc7de831815004098df5c4755e8f20f5df
Alexander Gryaznov <hi@agryaznov.com>
2025-03-24 11:47:49 +0200

    rm dbg

--- chronicle/src/shards/service.rs
+++ chronicle/src/shards/service.rs
@@ -494,7 +494,6 @@ where
 				data = block_stream.next() => {
 					if let Some(index) = data {
 						self.block_height = index;
-						tracing::info!("NEXT FIN BLOCK: #{index}");
 						if let Err(e) = self.admin_request.send(AdminMsg::TargetBlockReceived).await {
 							event!(
 								parent: span,
@@ -502,8 +501,6 @@ where
 								"Admin request error: {e:?}",
 							);
 						};
-					} else {
-						tracing::warn!("NEXT FIN BLOCK: None");
 					}
 				}
 			}
--- gmp/evm/src/lib.rs
+++ gmp/evm/src/lib.rs
@@ -189,7 +189,6 @@ impl IConnector for Connector {
 		blocks: Range<u64>,
 		cctp_info: Option<(Vec<Address32>, String)>,
 	) -> Result<Vec<GmpEvent>> {
-		let span = span!(Level::INFO, "STARTED read_events",);
 		let contract = a_addr(gateway);
 		let filter = Filter::new()
 			.address(contract)
@@ -200,7 +199,6 @@ impl IConnector for Connector {
 
 		let mut events = vec![];
 		for outer_log in logs {
-			tracing::info!(parent: &span, "ENTER stream loop");
 			let topics =
 				outer_log.topics().iter().map(|topic| B256::from(topic.0)).collect::<Vec<_>>();
 			let log = alloy::primitives::Log::new(
@@ -264,15 +262,12 @@ impl IConnector for Connector {
 					_ => {},
 				}
 			}
-			tracing::info!(parent: &span, "EXIT stream loop");
 		}
-		tracing::info!(parent: &span, "BEFORE CCTP");
 		// CCTP calls processing
 		let msgs = self.process_cctp_queue().await;
 		for msg in msgs {
 			events.push(GmpEvent::MessageReceived(msg));
 		}
-		tracing::info!(parent: &span, "AFTER CCTP");
 		Ok(events)
 	}
 	/// Submits a gmp message to the target chain.

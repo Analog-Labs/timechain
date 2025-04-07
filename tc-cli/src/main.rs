99d5c429c867590541b9a2e1c731eb1b3ba56255
David Craven <david@craven.ch>
2025-04-07 15:54:56 +0200

    Remove directives.

--- tc-cli/src/main.rs
+++ tc-cli/src/main.rs
@@ -227,9 +227,7 @@ async fn main() {
 }
 
 async fn real_main() -> Result<()> {
-	let filter = EnvFilter::from_default_env()
-		.add_directive("tc_cli=info".parse().unwrap())
-		.add_directive("gmp_evm=info".parse().unwrap());
+	let filter = EnvFilter::from_default_env();
 	tracing_subscriber::fmt().with_env_filter(filter).init();
 	let sender = Sender::new();
 	let args = Args::parse();

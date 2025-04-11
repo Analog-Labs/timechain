38d22c676a74b5103ab29a13c3b04eaf40aac1d2
4meta5 <asinghchrony@protonmail.com>
2025-04-10 22:50:04 -0400

    fix filters

--- runtime/src/configs/staking.rs
+++ runtime/src/configs/staking.rs
@@ -324,7 +324,7 @@ impl pallet_staking::Config for Runtime {
 	type WeightInfo = pallet_staking::weights::SubstrateWeight<Runtime>;
 	type OldCurrency = Balances;
 	type RuntimeHoldReason = RuntimeHoldReason;
-	type Filter = frame_support::traits::Everything;
+	type Filter = frame_support::traits::Nothing;
 }
 parameter_types! {
 	pub const BagThresholds: &'static [u64] = &crate::staking_bags::THRESHOLDS;
@@ -391,7 +391,7 @@ impl pallet_nomination_pools::Config for Runtime {
 	type MaxPointsToBalance = MaxPointsToBalance;
 	type AdminOrigin = DefaultAdminOrigin;
 	type BlockNumberProvider = System;
-	type Filter = frame_support::traits::Everything;
+	type Filter = frame_support::traits::Nothing;
 }
 
 parameter_types! {

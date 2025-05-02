cc717624685ec23ff2f1f30552f55b4698b5bb43
Haider-Ali-DS <haiderali.tech_786@outlook.com>
2025-05-02 17:46:25 +0500

    add test for zenswap

--- e2e-tests/tests/gateway.rs
+++ e2e-tests/tests/gateway.rs
@@ -1,5 +1,5 @@
 use anyhow::Result;
-use e2e_tests::{Backend, TestEnv, Tester};
+use e2e_tests::{TestEnv, Tester, TestingBackend};
 use futures::StreamExt;
 use std::collections::HashSet;
 
@@ -53,6 +53,6 @@ async fn gateway_payments() -> Result<()> {
 #[tokio::test]
 #[ignore] // TODO
 async fn gateway_payments_evm_tss() -> Result<()> {
-	let (_env, tc) = TestEnv::new(Backend::Evm, true).await?;
+	let (_env, tc) = TestEnv::new(TestingBackend::evm_local(), true).await?;
 	test_gateway_payments(tc).await
 }

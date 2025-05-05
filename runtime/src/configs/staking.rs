4e02fced9a55b301b292deaf16d813645268dc5e
omahs <73983677+omahs@users.noreply.github.com>
2025-05-05 16:48:18 +0200

    fix typo

--- runtime/src/configs/staking.rs
+++ runtime/src/configs/staking.rs
@@ -109,7 +109,7 @@ parameter_types! {
 	pub MaxActiveValidators: u32 = 1000;
 }
 
-/// The numbers configured here could always be more than the the maximum limits of staking pallet
+/// The numbers configured here could always be more than the maximum limits of staking pallet
 /// to ensure election snapshot will not run out of memory. For now, we set them to smaller values
 /// since the staking is bounded and the weight pipeline takes hours for this single pallet.
 pub struct ElectionProviderBenchmarkConfig;

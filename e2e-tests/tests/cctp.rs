a4814cbd350822d700e92574dd2f001f91bc04dd
David Craven <david@craven.ch>
2025-05-05 09:53:52 +0200

    Ignore cctp test.

--- e2e-tests/tests/cctp.rs
+++ e2e-tests/tests/cctp.rs
@@ -33,6 +33,7 @@ async fn cctp() -> Result<()> {
 }
 
 #[tokio::test]
+#[ignore]
 async fn cctp_evm() -> Result<()> {
 	let (_env, tc) = TestEnv::new(Backend::Evm, false).await?;
 	test_cctp(tc).await

beb212cc8a5488639e4d2c9c1379d55c1f919d0b
Florian Franzen <Florian.Franzen@gmail.com>
2025-05-13 07:58:00 +0200

    launch: add investor snapshot 1

---
+++ pallets/launch/src/data/v33.rs
@@ -0,0 +1,9 @@
+use crate::deposits::RawDepositStage;
+
+use time_primitives::ANLOG;
+
+pub const PRIVATE1_SNAPSHOT_1: RawDepositStage = &[
+	("an7AUybsFZbYRNjwCBfzkZcVMqedmXg5AANUB8nm68pb5ZCqy", 20_108_696 * ANLOG),
+	("an66K3mndKpJD4GpCcjUePJzarivhSyxcg1rLTxVPkSkx8oNY", 36_231_884 * ANLOG),
+	("an8Ufvs6xtGWqvLr5AykTHNv6PmEHrcGp4tkXnamHWKfR6Ncs", 27_173_913 * ANLOG),
+];

16c8d3a367725f52f7f72f6417ce6752b3d7c433
Florian Franzen <Florian.Franzen@gmail.com>
2025-05-13 05:51:53 +0200

    staking: update polkadot-sdk to disallow vested funds (#1863)

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -87,6 +87,7 @@
 // The runtime is split into its components
 pub mod apis;
 pub mod configs;
+pub mod migrations;
 pub mod offchain;
 pub mod version;
 
@@ -643,7 +644,19 @@ mod runtime {
 	pub type Revive = pallet_revive;
 }
 
-// All migrations executed on runtime upgrade implementing `OnRuntimeUpgrade`.
+// All migrations executed on runtime upgrade on mainnet
+#[cfg(not(any(feature = "testnet", feature = "develop")))]
+type Migrations = (
+	migrations::ExtendedProviderMigration<Runtime>,
+	pallet_staking::migrations::v16::MigrateV15ToV16<Runtime>,
+	pallet_session::migrations::v1::MigrateV0ToV1<
+		Runtime,
+		pallet_staking::migrations::v17::MigrateDisabledToSession<Runtime>,
+	>,
+);
+
+// All migrations executed on runtime upgrade everywhere else.
+#[cfg(any(feature = "testnet", feature = "develop"))]
 type Migrations = (
 	pallet_staking::migrations::v16::MigrateV15ToV16<Runtime>,
 	pallet_session::migrations::v1::MigrateV0ToV1<

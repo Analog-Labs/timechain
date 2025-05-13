91574cad7572e611928000b6bcd7289eebe6266b
Florian Franzen <Florian.Franzen@gmail.com>
2025-05-13 14:46:55 +0200

    launch: run stage 36 as well (#1869)

--- pallets/launch/src/lib.rs
+++ pallets/launch/src/lib.rs
@@ -69,7 +69,7 @@ pub mod pallet {
 	}
 
 	/// Updating this number will automatically execute the next launch stages on update
-	pub const LAUNCH_VERSION: u16 = 35;
+	pub const LAUNCH_VERSION: u16 = 36;
 	/// Wrapped version to support substrate interface as well
 	pub const STORAGE_VERSION: StorageVersion = StorageVersion::new(LAUNCH_VERSION);
 

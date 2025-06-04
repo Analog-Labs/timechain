0de8f986f43eaab454eab1772894e5463022434a
David Craven <david@craven.ch>
2025-06-04 09:30:48 +0200

    install packages in build.rs

--- gmp/evm/build.rs
+++ gmp/evm/build.rs
@@ -1,6 +1,13 @@
 fn main() {
 	let status = std::process::Command::new("forge")
 		.current_dir("gateway")
+		.arg("soldeer")
+		.arg("install")
+		.status()
+		.expect("failed to run soldeer");
+	assert!(status.success(), "soldeer exited with code 1");
+	let status = std::process::Command::new("forge")
+		.current_dir("gateway")
 		.arg("build")
 		.status()
 		.expect("failed to run forge");

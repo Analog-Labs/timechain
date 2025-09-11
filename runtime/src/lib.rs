e859c073a0e21e8b4852ffe70fd004443a64d09a
Florian Franzen <Florian.Franzen@gmail.com>
2025-09-11 13:13:03 +0200

    runtime: minor schedule fix

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -87,6 +87,7 @@
 // The runtime is split into its components
 pub mod apis;
 pub mod configs;
+pub mod migrations;
 pub mod offchain;
 pub mod version;
 
@@ -650,7 +651,7 @@ mod runtime {
 }
 
 // All migrations executed on runtime upgrade.
-type Migrations = ();
+type Migrations = (migrations::ScheduleCorrectionMigration<Runtime>,);
 
 #[cfg(test)]
 mod core_tests {

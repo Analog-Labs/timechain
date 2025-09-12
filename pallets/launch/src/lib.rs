6960a0dc0aa9530f9fb7d00441d1cbc7b610d950
Florian Franzen <Florian.Franzen@gmail.com>
2025-09-12 21:27:49 +0200

    runtime: remove old migration

--- pallets/launch/src/lib.rs
+++ pallets/launch/src/lib.rs
@@ -29,7 +29,7 @@ mod stage;
 
 use airdrops::AirdropBalanceOf;
 use allocation::Allocation;
-pub use deposits::{BalanceOf, CurrencyOf};
+use deposits::{BalanceOf, CurrencyOf};
 use ledger::{LaunchLedger, RawLaunchLedger};
 use stage::Stage;
 
--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -87,7 +87,6 @@
 // The runtime is split into its components
 pub mod apis;
 pub mod configs;
-pub mod migrations;
 pub mod offchain;
 pub mod version;
 
@@ -651,7 +650,7 @@ mod runtime {
 }
 
 // All migrations executed on runtime upgrade.
-type Migrations = (migrations::ScheduleCorrectionMigration<Runtime>,);
+type Migrations = ();
 
 #[cfg(test)]
 mod core_tests {

d8dd103a63c5ef3e8b31d79f8692fe5d9319a0d7
Florian Franzen <Florian.Franzen@gmail.com>
2025-10-14 10:30:25 +0200

    launch: add investor snapshot 13

--- pallets/launch/src/data/mod.rs
+++ pallets/launch/src/data/mod.rs
@@ -1,6 +1,3 @@
 // Include data files here
-pub mod v69;
-pub mod v70;
-pub mod v71;
-pub mod v72;
-pub mod v73;
+pub mod v74;
+pub mod v75;
--- pallets/launch/src/data/v71.rs
+++
@@ -1,6 +0,0 @@
-use crate::deposits::RawDepositStage;
-
-use time_primitives::ANLOG;
-
-pub const PRIVATE1_SNAPSHOT_12: RawDepositStage =
-	&[("an7dMNiiMP3KuPQGkw4hB4Ufy4zBtU6DzSX2MYGnBDh16E36x", 9_057_971 * ANLOG)];

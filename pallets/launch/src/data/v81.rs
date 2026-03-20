8cae323c25cfcca0a199cf3da097b40f5e29c1db
Haider Ali <haiderali.tech_786@outlook.com>
2026-03-20 09:47:57 +0500

    init

---
+++ pallets/launch/src/data/v81.rs
@@ -0,0 +1,6 @@
+use crate::deposits::RawDepositStage;
+
+use time_primitives::ANLOG;
+
+pub const OPPORTUNISTIC4_SNAPSHOT_16: RawDepositStage =
+	&[("an5s9VGeqXaDZbx4D4vxtiXT6LdxYTSPBZUww6AeCu1jvQdH7", 4_528_986 * ANLOG)];
---
+++ pallets/launch/src/data/v84.rs
@@ -0,0 +1,6 @@
+use crate::deposits::RawDepositStage;
+
+use time_primitives::ANLOG;
+
+pub const PRIVATE1_SNAPSHOT_16: RawDepositStage =
+	&[("an6oGBdayY68qnb7gk8Rd9vRGca3DEY1NP4JC5YkrVRkWHzSC", 181_159_420 * ANLOG)];

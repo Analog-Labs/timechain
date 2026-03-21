2c302cbbbcdf395179d00acff6308b879486c121
Haider Ali <haiderali.tech_786@outlook.com>
2026-03-22 01:57:45 +0500

    fix op4 block

--- pallets/launch/src/data/v81.rs
+++ pallets/launch/src/data/v81.rs
@@ -8,10 +8,11 @@ pub const OPPORTUNITY4_SNAPSHOT_16: RawVestedDepositStage = &[(
 	// 10 % TGE = 452_898.6 ANLOG
 	// remaining = 4_076_087.4 ANLOG
 	4_076_087_400 * MILLIANLOG,
-	// reference PR: 1974
-	// 10% TGE was already sent manually. Remaining = 90% = 4,076,087.4 ANLOG
-	// Vesting: 12 months linear, start = Opportunity4 TGE block (2,008,470)
-	// total_vesting_blocks = 12 * 439,200 = 5,270,400
+	// OP4 block = 5_618
+	// delay block = 690_870
+	// start block = 696_488
+
+	// total_vesting_blocks = 12 * 439,200(blocks in a month) = 5,270,400
 	// per_block = 4,076,087.4 ANLOG / 5,270,400 = 0.773392 ANLOG
-	Some((4_076_087_400 * MILLIANLOG, 773 * MILLIANLOG, 2_008_470)),
+	Some((4_076_087_400 * MILLIANLOG, 773 * MILLIANLOG, 696_488)),
 )];
